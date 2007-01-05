8128d7fd56128453a280d30aa4dc45e677eb12e5
martyn_plummer <devnull@localhost>
2007-01-05 14:59:46 +0000

    Remove numerical fudging in inverse logit function

--- src/modules/bugs/functions/ILogit.cc
+++ src/modules/bugs/functions/ILogit.cc
@@ -4,31 +4,16 @@
 #include <cmath>
 #include <cfloat>
 
-#include <JRmath.h>
-
 using std::vector;
 
-ILogit::ILogit ():InverseLinkFunc ("ilogit", "logit")
+ILogit::ILogit ()
+    : InverseLinkFunc ("ilogit", "logit")
 {
 }
 
-double
-ILogit::eval (vector<double const *> const &args) const
+double ILogit::eval (vector<double const *> const &args) const
 {
-  /* Returning 0 or 1 can cause numerical problems. A result
-     of 0 or 1 may just be the result of numerical imprecision.
-     What we really want to return is e  or (1 - e) for very
-     small e */
-
-  double q = *args[0];
-  double y = exp (-q);
-  if (!R_FINITE (y)) {
-    return DBL_EPSILON;
-  }
-  else {
-    double p =  1 / (1 + y);
-    return p == 1 ? 1 - DBL_EPSILON : p;
-  }
+    return 1/(1 + exp(-args[0][0]));
 }
 
 double ILogit::link(double mu) const
