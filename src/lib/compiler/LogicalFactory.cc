eb16f6a8104938f43a018e1087eb6ab16ada5c80
martyn_plummer <devnull@localhost>
2007-01-07 17:01:23 +0000

    Allow recycling of observed stochastic nodes with identical distributions and values.

--- src/lib/compiler/LogicalFactory.cc
+++ src/lib/compiler/LogicalFactory.cc
@@ -12,79 +12,7 @@ using std::map;
 using std::vector;
 using std::invalid_argument;
 
-#define TOL 16 * DBL_EPSILON
 
-/* Comparison operator for Nodes */
-static bool lt(Node const *node1, Node const *node2)
-{
-    if (node1 == node2) {
-	// A node is always identical to itself
-	return false; 
-    }
-
-    if (node1->isObserved() && !node2->isObserved()) {
-	//Observed nodes come before unobserved nodes
-	return true;
-    }
-    else if (!node1->isObserved() && node2->isObserved()) {
-	return false;
-    }
-    else if (node1->isObserved() && node2->isObserved()) {
-	//Observed nodes are sorted by dimension, then value
-	if (node1->dim() < node2->dim()) {
-	    return true;
-	}
-	else if (node1->dim() > node2->dim()) {
-	    return false;
-	}
-	else {
-	    //Equal length observed nodes are sorted by value, with
-	    //some numerical tolerance
-	    unsigned long n = node1->length();
-	    double const *value1 = node1->value(0);
-	    double const *value2 = node2->value(0);
-	    for (unsigned long i = 0; i < n; ++i) {
-		if (value1[i] < value2[i] - TOL) {
-		    return true;
-		}
-		else if (value1[i] > value2[i] + TOL) {
-		    return false;
-		}
-	    }
-	    return false; //Numerically identical observed nodes
-	}
-    }
-    else {
-	//Unobserved nodes are sorted by address. The ordering is
-	//arbitrary, but unique.
-	return (node1 < node2);
-    }
-}
-
-/* Comparison operator for vectors of parameters */
-static bool 
-lt(vector<Node const *> const &par1, vector<Node const *> const &par2)
-{
-    //Order by size
-    if (par1.size() < par2.size()) {
-	return true;
-    }
-    else if (par1.size() > par2.size()) {
-	return false;
-    }
-    else {
-        //Equal sized vectors: Sort by ordering of elements 
-	for (unsigned int i = 0; i < par1.size(); ++i) {
-	    if (lt(par1[i], par2[i])) {
-		return true;
-	    }
-	    else if (lt(par2[i], par1[i])) {
-		return false;
-	    }
-	}
-	return false;
-    }
-}
 
 bool lt(LogicalPair const &arg1, LogicalPair const &arg2)
 {
@@ -111,7 +39,8 @@ LogicalFactory::getLogicalNode(Function const *func,
   }
 
   LogicalPair lpair(func, parents);
-  map<LogicalPair, LogicalNode*>::iterator i = _logicalmap.find(lpair);
+  map<LogicalPair, LogicalNode*, ltlogical>::iterator i 
+      = _logicalmap.find(lpair);
 
   if (i != _logicalmap.end()) {
     return i->second;
