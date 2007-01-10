a545ac16e53387a6cce3e042e1d7ca248eb1d2c7
martyn_plummer <devnull@localhost>
2007-01-10 17:55:04 +0000

    MixSampler as a random walk Metropolis sampler.

--- src/modules/mix/samplers/MixSampler.h
+++ src/modules/mix/samplers/MixSampler.h
@@ -1,8 +1,15 @@
 #include <sampler/Sampler.h>
 
+#include <vector>
+
 class MixSampler : public Sampler
 {
+    const unsigned int _length;
+    double const *_step;
+    double const *_value;
+public:
     MixSampler();
     void update(RNG *rng);
     void burninOff();
-}
+    void setValue(double const *value, unsigned int length);
+};
--- src/modules/mix/samplers/MixSamplerFactory.cc
+++ src/modules/mix/samplers/MixSamplerFactory.cc
@@ -1,47 +1,93 @@
 #include <config.h>
-
+#include "MixSamplerFactory.h"
+#include "MixSampler.h"
 
 MixSamplerFactory::MixSamplerFactory()
 {}
 
-void
-MixSamplerFactory::makeSampler(std::set<StochasticNode*> &nodes, 
-			       Graph const &graph,
-			       std::vector<std::vector<Sampler*> > &samplers) 
-    const
+static bool 
+hasMarkedChild(Node *node, Graph const &graph, GraphMarks const &marks)
 {
-
-    /* Find Stochastic Nodes in the graph with distribution DNormMix
-       and add them to the set "mix_nodes" */
-
-    set<StochasticNode*> mix_nodes;
-    set<Node*>::const_iterator p;
-    for (p = graph.nodes().begin(); p != graph.nodes().end(); ++p) {
-	StochasticNode const *snode = asStochastic(*p);
-	if (snode && snode->distribution()->name() == "DNormMix") {
-	    mix_nodes.insert(snode);
+    set<Node*>::const_iterator i = node->children().begin();
+    for (; i != node->children().end(); ++i) {
+	if (graph.contains(*i) && marks.mark(*i) != 0) {
+	    return true;
 	}
     }
-    
-    for (set<StochasticNode*>::iterator i = mix_nodes.begin(); 
-	 i != mix_nodes.end(); ++i)
-    {
-	StochasticNode *snode = *i;
-	
-	GraphMarks marks(graph);
-	marks.markAncestors(snode,2);
-	vector<StochasticNode *> sample_nodes;
-	for (p = graph.nodes().begin(); p != graph.nodes().end(); ++p) {
-	    if (marks.mark(*p) == 2) {
-		StochasticNode *sample_node_p = asStochastic(*p);
-		if (sample_node_p) {
-		    sample_nodes.push_back(sample_node_p);
-		}
+    return false;
+}
+
+void MixSamplerFactory::makeSampler(set<StochasticNode*> &nodes, 
+				    Graph const &graph,
+				    vector<vector<Sampler*> > &samplers) const
+{
+    GraphMarks marks(graph);
+
+    /* Find observed nodes in the graph and mark their ancestors:
+       these are the informative nodes */
+    set<Node*>::const_iterator p;
+    for (p = graph.nodes().begin(); p != graph.nodes().end(); ++p) {
+	if ((*p)->isObserved()) {
+	    marks.mark(*p, 1);
+	    marks.markAncestors(*p, 1);
+	}
+    }
+
+    /* Find informative nodes in the graph with distribution DNormMix
+       and add them to the vector "mix_nodes" */
+    vector<StochasticNode*> mix_nodes;
+    for (p = graph.nodes().begin(); p != graph.nodes().end(); ++p) {
+	if (marks.mark(*p) == 1) {
+	    StochasticNode const *snode = asStochastic(*p);
+	    if (snode && snode->distribution()->name() == "DNormMix") {
+		mix_nodes.push_back(snode);
 	    }
 	}
+    }
+    if (mix_nodes.empty()) {
+	return; //Nothing to do
+    }
 
-	
-	
-	
+    /* Now find the stochastic parents of mix_nodes */
+    for (unsigned int i = 0; i < mix_nodes.size(); ++i) {
+	marks.markParents(mix_nodes[i], isStochastic, 2);
+    }
+    /* The stochastic parents can be sampled if
+       1) They are not discrete valued
+       2) They have fixed support
+    
+       The second condition is necessary because the sampler uses log
+       or logit transformation of bounded variables, which must remain
+       constant between iterations to ensure reversibility of the
+       chain.
+    */
+    vector<StochasticNode*> sample_nodes;
+    for (p = graph.nodes().begin(); p != graph.nodes().end(); ++p) {
+	if (marks.Mark(*p) == 2) {
+	    StochasticNode *snode = asStochastic(*p);
+	    if (!snode->isDiscreteValued()) {
+		unsigned int Nparents = snode->parents().size();
+		vector<bool> fixmask(Nparents);
+		for (unsigned int i = 0; i < Nparents; i++) {
+		    fixmask[i] = snode->parents()[i]->isObserved();
+		}
+		if (snode->distribution()->isSupportFixed(fixmask)) {
+		    sample_nodes.push_back(snode);
+		}
+	    }
+	    
+	}
+    }
+
+    if (sample_nodes.empty()) {
+	return; //Nothing to do
+    }
+    else if (DSumSampler::canSample(sample_nodes, graph)) {
+	for (unsigned int i = 0; i < sample_nodes.size(); ++i) {
+	    nodes.erase(sample_nodes[i]);
+	}
+	for (unsigned int ch = 0; ch < samplers.size(); ++ch) {
+	    samplers[ch].push_back(new MixSampler(sample_nodes, graph, ch));
+	}
     }
 }
