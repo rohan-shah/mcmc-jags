a79bbe9832d78f9c0494dab87615551ba6bd8473
martyn_plummer <devnull@localhost>
2007-01-24 17:15:37 +0000

    Added files for Random Effects sampler

---
+++ src/modules/bugs/samplers/REFactory.h
@@ -0,0 +1,25 @@
+#ifndef RANDOM_EFFECT_FACTORY_H_
+#define RANDOM_EFFECT_FACTORY_H_
+
+#include <sampler/SamplerFactory.h>
+class StochasticNode;
+class Graph;
+
+/**
+ * @short Factory object for random effect samplers
+ *
+ * Random effects are defined as a set of N > 20 unobserved stochastic
+ * nodes with mean 0 and a common variance. The RandomEffectSampler
+ * updates the effects and their variance simultaneously.
+ * 
+ */
+class REFactory : public SamplerFactory
+{
+public:
+    virtual void makeSampler(std::set<StochasticNode*> &nodes, 
+			     Graph const &graph,
+			     std::vector<std::vector<Sampler*> > &samplers)
+	const;
+};
+
+#endif /* RANDOM_EFFECT_FACTORY_H_ */
