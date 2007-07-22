0b7968d66d99a5d426914046fc241d4c0ecc5845
martyn_plummer <devnull@localhost>
2007-07-22 14:16:52 +0000

    Moded "pow()" function from basefunctions module

---
+++ src/modules/bugs/functions/Pow.cc
@@ -0,0 +1,31 @@
+#include <config.h>
+#include "Pow.h"
+
+#include <cmath>
+#include <cfloat>
+
+using std::vector;
+using std::fabs;
+using std::pow;
+
+Pow::Pow ()
+  : ScalarFunc ("pow", 2)
+{
+}
+
+double Pow::eval(vector<double const *> const &args) const
+{
+    return pow (*args[0], *args[1]);
+}
+
+bool Pow::checkParameterValue(vector<double const *> const &args) const
+{
+    if (*args[0] >= 0) {
+	return true;
+    }
+    else {
+	double arg2 = *args[1];
+	int iarg2 = static_cast<int>(arg2 + DBL_EPSILON);
+	return fabs(arg2 - iarg2) < DBL_EPSILON;
+    }
+}
