74844fdbddd30f99dd0144709d277d012c581196
martyn_plummer <devnull@localhost>
2007-08-12 10:38:38 +0000

    FiniteSampler.* renamed to FiniteMethod.* to match new class name. FiniteMethod class modified to no longer require initialization.

--- src/modules/basesamplers/FiniteFactory.cc
+++ src/modules/basesamplers/FiniteFactory.cc
@@ -14,41 +14,9 @@ using std::vector;
 namespace basesamplers {
 
     bool 
-    FiniteFactory::canSample(StochasticNode * node, Graph const &graph) const
+    FiniteFactory::canSample(StochasticNode *snode, Graph const &graph) const
     {
-	//Node must be scalar with discrete-valued distribution of full rank
-	Distribution const *dist = node->distribution();
-	if (!dist->isDiscreteValued())
-	    return false;
-
-	if (node->length() != 1)
-	    return false;
-
-	if (df(node) == 0)
-	    return false;
-
-	for (unsigned int ch = 0; ch < node->nchain(); ++ch) {
-	    //Distribution cannot be unbounded
-	    double ulimit = 0, llimit = 0;
-	    dist->support(&llimit, &ulimit, 1, node->parameters(ch), 
-			  node->parameterDims());
-	    if (!jags_finite(ulimit) || !jags_finite(llimit))
-		return false;
-
-	    //We don't want too many possibilities
-	    double n = ulimit - llimit + 1;
-	    if (n <= 1 || n > 20) //fixme: totally arbitrary
-		return false;
-
-	    //Support must be fixed
-	    vector<bool> fixmask(node->parents().size());
-	    for (unsigned int i = 0; i < node->parents().size(); ++i) {
-		fixmask[i] = node->parents()[i]->isObserved();
-	    }
-	    if (!node->distribution()->isSupportFixed(fixmask))
-		return false;
-	}
-	return true;
+	return FiniteMethod::canSample(snode);
     }
 
     Sampler *  FiniteFactory::makeSingletonSampler(StochasticNode *snode,
@@ -57,11 +25,11 @@ namespace basesamplers {
 	unsigned int nchain = snode->nchain();
 	vector<DensityMethod*> methods(nchain, 0);
 	for (unsigned int i = 0; i < nchain; ++i) {
-	    methods[i] = new FiniteSampler;
+	    methods[i] = new FiniteMethod(snode);
 	}
 	
-	vector<StochasticNode*> nodes(1, snode);
-	return new ParallelDensitySampler(nodes, graph, methods);
+	vector<StochasticNode*> sample_nodes(1, snode);
+	return new ParallelDensitySampler(sample_nodes, graph, methods);
     }
     
 }
---
+++ src/modules/basesamplers/FiniteMethod.cc
@@ -0,0 +1,110 @@
+#include <config.h>
+#include <distribution/Distribution.h>
+#include <graph/StochasticNode.h>
+#include <sampler/ParallelDensitySampler.h>
+#include <rng/RNG.h>
+#include <sarray/nainf.h>
+#include <graph/NodeError.h>
+#include "FiniteSampler.h"
+
+#include <cmath>
+#include <string>
+#include <stdexcept>
+#include <vector>
+
+using std::logic_error;
+using std::string;
+using std::vector;
+using std::exp;
+
+namespace basesamplers {
+
+    FiniteMethod::FiniteMethod(StochasticNode const *snode)
+    {
+	if (!canSample(snode)) {
+	    throw logic_error("Invalid FiniteMethod");
+	}
+
+	Distribution const *dist = snode->distribution();
+	
+	double lower = 0, upper = 0;
+	dist->support(&lower, &upper, 1, snode->parameters(0), 
+		      snode->parameterDims());
+	_lower = static_cast<int>(lower);
+	_upper = static_cast<int>(upper);
+    }
+    
+    void FiniteMethod::update(RNG *rng)
+    {
+	int size = _upper - _lower + 1;
+	double *lik = new double[size];
+	double liksum = 0.0;
+	for (int i = 0; i < size; i++) {
+	    double ivalue = _lower + i;
+	    _sampler->setValue(&ivalue, 1, _chain);
+	    lik[i] = exp(_sampler->logFullConditional(_chain));
+	    liksum += lik[i];
+	}
+
+	if (liksum == 0 || !jags_finite(liksum)) {
+            Node *node = _sampler->nodes()[0];
+	    throw NodeError(node, "Cannot normalize density in FiniteMethod");
+	}
+
+	/* Sample */
+	double urand = rng->uniform() * liksum;
+	int i;
+	liksum = 0.0;
+	for (i = 0; i < size - 1; i++) {
+	    liksum += lik[i];
+	    if (liksum > urand) {
+		break;
+	    }
+	}
+	double ivalue = _lower + i;
+	_sampler->setValue(&ivalue, 1, _chain);
+	delete [] lik;
+    }
+
+    bool FiniteMethod::adaptOff()
+    {
+	return true;
+    }
+    
+    bool FiniteMethod::canSample(StochasticNode const * node)
+    {
+	//Node must be scalar with discrete-valued distribution of full rank
+	Distribution const *dist = node->distribution();
+	if (!dist->isDiscreteValued())
+	    return false;
+
+	if (node->length() != 1)
+	    return false;
+
+	if (df(node) == 0)
+	    return false;
+
+	for (unsigned int ch = 0; ch < node->nchain(); ++ch) {
+	    //Distribution cannot be unbounded
+	    double ulimit = 0, llimit = 0;
+	    dist->support(&llimit, &ulimit, 1, node->parameters(ch), 
+			  node->parameterDims());
+	    if (!jags_finite(ulimit) || !jags_finite(llimit))
+		return false;
+
+	    //We don't want too many possibilities
+	    double n = ulimit - llimit + 1;
+	    if (n <= 1 || n > 20) //fixme: totally arbitrary
+		return false;
+
+	    //Support must be fixed
+	    vector<bool> fixmask(node->parents().size());
+	    for (unsigned int i = 0; i < node->parents().size(); ++i) {
+		fixmask[i] = node->parents()[i]->isObserved();
+	    }
+	    if (!node->distribution()->isSupportFixed(fixmask))
+		return false;
+	}
+	return true;
+    }
+}
