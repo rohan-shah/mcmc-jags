873727027fdbfc80adfbab7c0c7ae9fecab01ca4
martyn_plummer <devnull@localhost>
2007-08-12 09:26:21 +0000

    Major overhaul of the Sampler class. Instead of creating many subclasses of Sampler, updating is done with a new class of object that acts on a Sampler. For example, ParallelSampler, which inherits from Sampler, uses a DensityMethod object. Subclasses of DensityMethod include Slicer and Metropolis. Entirely within the bugs module, a subclass ConjugateSampler is defined, which uses a single stateless ConjugateMethod object to update all chains.

--- src/lib/sampler/SingletonFactory.cc
+++ src/lib/sampler/SingletonFactory.cc
@@ -7,14 +7,12 @@ using std::set;
 
 void SingletonFactory::makeSampler(set<StochasticNode*> &nodes, 
 				   Graph const &graph,
-				   vector<vector<Sampler*> > &samplers) const
+				   vector<Sampler*> &samplers) const
 {
     for (set<StochasticNode*>::iterator p(nodes.begin()); p != nodes.end(); )
     {
 	if (canSample(*p, graph)) {
-	    for (unsigned int n = 0; n < samplers.size(); ++n) {
-		samplers[n].push_back(makeSingletonSampler(*p, graph, n));
-	    }
+	    samplers.push_back(makeSingletonSampler(*p, graph));
 	    nodes.erase(p++); //Avoid invalidating the iterator
 	}
 	else {
