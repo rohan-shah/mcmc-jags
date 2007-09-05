f639d7ccd913e0ae9dc151dedecab5f1beb068f2
martyn_plummer <devnull@localhost>
2007-09-05 22:03:36 +0000

    Simplified Monitor API. Added PDMonitor.

--- src/modules/dic/DevianceMonitorFactory.cc
+++ src/modules/dic/DevianceMonitorFactory.cc
@@ -7,13 +7,16 @@
 #include <set>
 
 using std::set;
+using std::string;
+using std::vector;
 
 namespace dic {
 
     Monitor *DevianceMonitorFactory::getMonitor(Node const *node, 
+						Model *model,
 						unsigned int start,
 						unsigned int thin, 
-						std::string const &type)
+						string const &type)
     {
 	if (type != "deviance")
 	    return 0;
@@ -21,34 +24,28 @@ namespace dic {
 	    return 0;
 
 	StochasticNode const *snode = asStochastic(node);
-	if (!snode)
+	if (snode)
+	    return new DevianceMonitor(snode, start, thin);
+	else 
 	    return 0;
-
-	return new DevianceMonitor(snode, start, thin);
     }
 
-    //FIXME: Who owns these monitors?
-    void DevianceMonitorFactory::addDefaultMonitors(Model *model,
-                                                    unsigned int thin,
-			                            std::string const &type)
+    vector<Node const*> 
+    DevianceMonitorFactory::defaultNodes(Model *model, 
+					 string const &type) const
     {
-        if (type != "deviance") {
-            return;
-        }
+	vector<Node const*> dnodes;
 
-	unsigned int start = model->iteration() + 1;
-
-	set<Node*> const &nodes = model->graph().nodes();
-	set<Node*>::const_iterator p = nodes.begin(); 
-	for ( ; p != nodes.end(); ++p) {
-	    if ((*p)->isObserved()) {
-		StochasticNode const *snode = asStochastic(*p);
-		if (snode) {
-		    Monitor *monitor = new DevianceMonitor(snode, start, thin);
-		    model->addMonitor(monitor);
+        if (type == "deviance") {
+	    set<Node*> const &nodes = model->graph().nodes();
+	    set<Node*>::const_iterator p = nodes.begin(); 
+	    for ( ; p != nodes.end(); ++p) {
+		if ((*p)->isObserved() && asStochastic(*p)) {
+		    dnodes.push_back(*p);
 		}
 	    }
 	}
+	return dnodes;
     }
 	
 }
--- src/modules/dic/DevianceMonitorFactory.h
+++ src/modules/dic/DevianceMonitorFactory.h
@@ -8,10 +8,11 @@ namespace dic {
 class DevianceMonitorFactory : public MonitorFactory
 {
 public:
-    Monitor *getMonitor(Node const *node, unsigned int start,
-			unsigned int thin, std::string const &type);
-    void addDefaultMonitors(Model *model, unsigned int thin,
-			    std::string const &type);
+    Monitor *getMonitor(Node const *node, Model *model,
+			unsigned int start, unsigned int thin, 
+			std::string const &type);
+    std::vector<Node const*> defaultNodes(Model *model,
+					  std::string const &type) const;
 };
 
 }
