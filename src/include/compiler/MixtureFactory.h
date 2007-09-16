5ed66006e10112f3da5f6421a386826787bf302f
martyn_plummer <devnull@localhost>
2007-09-16 22:44:03 +0000

    Documentation  updates Streamline behaviour of MixtureNodes

--- src/include/compiler/MixtureFactory.h
+++ src/include/compiler/MixtureFactory.h
@@ -11,20 +11,46 @@
 class NodeArray;
 class MixtureNode;
 
-typedef std::pair<std::vector <Node const*>, std::vector<std::pair<std::vector<int> , Node const *> > > 
-MixPair;
+/**
+ * A MixMap is an STL map object.  The key represents an integer-valued
+ * index, and the value is the corresponding Node from which a
+ * MixtureNode will copy its values when its indices take that value.
+ *
+ * @see Mix
+ */
+typedef std::map<std::vector<int> , Node const *>  MixMap;
 
+/**
+ * A MixPair contains the information required to uniquely identify a
+ * MixtureNode. The first element of the pair is a vector of Nodes
+ * (which should be discrete-valued) representing the indices of the
+ * MixtureNode. The second element is a MixMap which defines how each
+ * possible value of the indices is mapped onto a parent Node.
+ *
+ * @see compMixPair ltmixpair
+ */
+typedef std::pair<std::vector <Node const*>, MixMap> MixPair;
+
+/**
+ * Comparison function for MixPair objects which is used to give them
+ * a unique (but arbitrary) ordering. The compMixPair function returns
+ * true if the first argument comes before the second in the ordering.
+ *
+ * @see ltmixpair
+ */
 bool compMixPair(MixPair const &, MixPair const &);
 
 /**
  * @short STL function object for the map class using MixPair as a key
+ *
+ * @see MixtureFac
  */
 struct ltmixpair
 {
-  bool operator()(MixPair const &arg1, MixPair const &arg2) const
-  {
-    return compMixPair(arg1, arg2);
-  }
+    bool operator()(MixPair const &arg1, MixPair const &arg2) const
+    {
+	return compMixPair(arg1, arg2);
+    }
 };
 
 /**
@@ -54,9 +80,7 @@ public:
    *
    */
   MixtureNode *getMixtureNode(std::vector<Node const *> const &index,
-			      std::vector<std::pair<std::vector<int> , Node const *> > 
-			      const &parameters,
-                              Graph &graph);
+			      MixMap const &parameters, Graph &graph);
 };
 
 #endif /* MIXTURE_FACTORY_H_ */
