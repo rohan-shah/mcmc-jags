8146f2a161f7dc2c5c03f9149acbef199c22975d
martyn_plummer <devnull@localhost>
2007-09-20 21:42:33 +0000

    Adding files for base module

---
+++ src/modules/base/functions/Subtract.cc
@@ -0,0 +1,33 @@
+#include <config.h>
+#include <sarray/util.h>
+#include "Subtract.h"
+
+#include <stdexcept>
+
+using std::vector;
+using std::logic_error;
+
+namespace base {
+
+Subtract::Subtract () : Infix ("-")
+{
+}
+
+double
+Subtract::evaluateScalar(vector<double const *> const &args) const
+{
+    return *args[0] - *args[1];
+}
+
+bool Subtract::isDiscreteValued(vector<bool> const &mask) const
+{
+  return allTrue(mask);
+}
+
+bool 
+Subtract::isLinear(vector<bool> const &mask, vector<bool> const &fix) const
+{
+    return true;
+}
+
+}
