21c0c5476ad803b59a9117a4c3bca518061622ad
martyn_plummer <devnull@localhost>
2007-09-20 21:42:33 +0000

    Adding files for base module

---
+++ src/modules/base/functions/And.cc
@@ -0,0 +1,22 @@
+#include <config.h>
+#include "And.h"
+
+using std::vector;
+
+namespace base {
+
+And::And () : Infix ("&&")
+{
+}
+
+double And::evaluateScalar(vector <double const *> const &args) const
+{
+  return *args[0] && *args[1];
+}
+
+bool And::isDiscreteValued(std::vector<bool> const &mask) const
+{
+  return true;
+}
+
+}
---
+++ src/modules/base/functions/And.h
@@ -0,0 +1,28 @@
+#ifndef FUNC_AND_H_
+#define FUNC_AND_H_
+
+#include "Infix.h"
+
+namespace base {
+
+/**
+ * The And function implements the infix operator "&" in the BUGS 
+ * language
+ * @short Logical and 
+ * <pre>
+ * y <- a&b
+ * y = 1 if a != 0 and  b != 0
+ * y = 0 otherwise
+ * </pre>
+ */
+class And : public Infix
+{
+public:
+    And ();
+    double evaluateScalar(std::vector<double const *> const &args) const;
+    bool isDiscreteValued(std::vector<bool> const &mask) const;
+};
+
+}
+
+#endif /* FUNC_AND_H_ */
