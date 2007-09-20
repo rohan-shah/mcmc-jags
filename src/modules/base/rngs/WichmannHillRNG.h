4ed0cd520699555572b276a1f9dabda54a800484
martyn_plummer <devnull@localhost>
2007-09-20 21:42:34 +0000

    Adding files for base module

---
+++ src/modules/base/rngs/WichmannHillRNG.h
@@ -0,0 +1,22 @@
+#ifndef _WICHMANN_HILL_RNG_H_
+#define _WICHMANN_HILL_RNG_H_
+
+#include <rng/RmathRNG.h>
+
+namespace base {
+
+    class WichmannHillRNG : public RmathRNG
+    {
+	unsigned int I[3];
+	void fixupSeeds();
+    public:
+	WichmannHillRNG(unsigned int seed, NormKind norm_kind);
+	double uniform();
+	void init(unsigned int seed);
+	bool setState(std::vector<int> const &state);
+	void getState(std::vector<int> &state) const;
+    };
+
+}
+
+#endif /* _WICHMANN_HILL_RNG_H_ */
