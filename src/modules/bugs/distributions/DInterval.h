345f54aaf4cc18e558aaa922a9f1a6741549034d
martyn_plummer <devnull@localhost>
2007-09-22 20:50:57 +0000

    For trunctated nodes, the upper and lower bounds are no longer added to the vector of parameters. Instead the lower and upper bounds have been added as additional parameters to several important functions in the Distribution and StochasticNode classes.

--- src/modules/bugs/distributions/DInterval.h
+++ src/modules/bugs/distributions/DInterval.h
@@ -12,35 +12,37 @@
  * </pre>
  */
 class DInterval : public Distribution {
- public:
-  DInterval();
+public:
+    DInterval();
   
-  double logLikelihood(double const *x, unsigned int length,
-		       std::vector<double const *> const &parameters,
-		       std::vector<std::vector<unsigned int> > const &dims) 
-      const;
+    double logLikelihood(double const *x, unsigned int length,
+			 std::vector<double const *> const &parameters,
+			 std::vector<std::vector<unsigned int> > const &dims,
+			 double const *lower, double const *upper) const;
 
-  void randomSample(double *x, unsigned int length,
-		    std::vector<double const *> const &parameters,
-		    std::vector<std::vector<unsigned int> > const &dims,
-		    RNG *rng) const;
-  /**
-   * Checks that t is scalar and that cutpoints is a vector
-   */
-  bool checkParameterDim(std::vector<std::vector<unsigned int> > const &parameters) const;
-  /**
-   * Checks that cutpoints are in ascending order
-   */
-  bool checkParameterValue(std::vector<double const *> const &parameters,
-			   std::vector<std::vector<unsigned int> > const &dims) const;
-  void support(double *lower, double *upper, unsigned int length,
-	       std::vector<double const *> const &parameters,
-	       std::vector<std::vector<unsigned int> > const &dims) const;
-  void typicalValue(double *x, unsigned int length,
-		    std::vector<double const *> const &parameters,
-		    std::vector<std::vector<unsigned int> > const &dims) const;
-  bool isSupportFixed(std::vector<bool> const &fixmask) const;
-  unsigned int df(std::vector<std::vector<unsigned int> > const &dims) const;
+    void randomSample(double *x, unsigned int length,
+		      std::vector<double const *> const &parameters,
+		      std::vector<std::vector<unsigned int> > const &dims,
+		      double const *lower, double const *upper, RNG *rng) const;
+    void typicalValue(double *x, unsigned int length,
+		      std::vector<double const *> const &parameters,
+		      std::vector<std::vector<unsigned int> > const &dims,
+		      double const *lower, double const *upper) const;
+
+    /**
+     * Checks that t is scalar and that cutpoints is a vector
+     */
+    bool checkParameterDim(std::vector<std::vector<unsigned int> > const &parameters) const;
+    /**
+     * Checks that cutpoints are in ascending order
+     */
+    bool checkParameterValue(std::vector<double const *> const &parameters,
+			     std::vector<std::vector<unsigned int> > const &dims) const;
+    void support(double *lower, double *upper, unsigned int length,
+		 std::vector<double const *> const &parameters,
+		 std::vector<std::vector<unsigned int> > const &dims) const;
+    bool isSupportFixed(std::vector<bool> const &fixmask) const;
+    unsigned int df(std::vector<std::vector<unsigned int> > const &dims) const;
 
 };
 
