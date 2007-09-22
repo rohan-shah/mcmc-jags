58e50a81598a394ed01370cd04f05141201a5ce5
martyn_plummer <devnull@localhost>
2007-09-22 20:50:57 +0000

    For trunctated nodes, the upper and lower bounds are no longer added to the vector of parameters. Instead the lower and upper bounds have been added as additional parameters to several important functions in the Distribution and StochasticNode classes.

--- src/modules/bugs/distributions/DCat.h
+++ src/modules/bugs/distributions/DCat.h
@@ -12,33 +12,37 @@
  */
 class DCat : public Distribution {
 public:
-  DCat();
+    DCat();
 
-  double logLikelihood(double const *x, unsigned int length,
-		       std::vector<double const *> const &parameters,
-		       std::vector<std::vector<unsigned int> > const &dims)
-      const;
-  void randomSample(double *x, unsigned int length,
-		    std::vector<double const *> const &parameters,
-		    std::vector<std::vector<unsigned int> > const &dims,
-		    RNG *rng) const;
-  void support(double *lower, double *upper, unsigned int length,
-	       std::vector<double const *> const &parameters,
-	       std::vector<std::vector<unsigned int> > const &dims) const;
-  void typicalValue(double *x, unsigned int length,
-		    std::vector<double const *> const &parameters,
-		    std::vector<std::vector<unsigned int> > const &dims) const;
-  /**
-   * Checks that  p is a vector of length at least 2
-   */
-  bool checkParameterDim(std::vector<std::vector<unsigned int> > const &dims) 
-      const;
-  /**
-   * Checks that all elements of p are positive
-   */
-  bool checkParameterValue(std::vector<double const*> const &parameters,
-                    std::vector<std::vector<unsigned int> > const &dims) const;
-  bool isSupportFixed(std::vector<bool> const &fixmask) const;
+    double logLikelihood(double const *x, unsigned int length,
+			 std::vector<double const *> const &parameters,
+			 std::vector<std::vector<unsigned int> > const &dims,
+			 double const *lower, double const *upper) const;
+    void randomSample(double *x, unsigned int length,
+		      std::vector<double const *> const &parameters,
+		      std::vector<std::vector<unsigned int> > const &dims,
+		      double const *lbound, double const *ubound,
+		      RNG *rng) const;
+    void typicalValue(double *x, unsigned int length,
+		      std::vector<double const *> const &parameters,
+		      std::vector<std::vector<unsigned int> > const &dims,
+		      double const *lbound, double const *ubound) const;
+    /**
+     * Checks that  p is a vector of length at least 2
+     */
+    bool checkParameterDim(std::vector<std::vector<unsigned int> > const &dims) 
+	const;
+    /**
+     * Checks that all elements of p are positive
+     */
+    bool 
+	checkParameterValue(std::vector<double const*> const &parameters,
+			    std::vector<std::vector<unsigned int> > const &dims)
+	const;
+    void support(double *lower, double *upper, unsigned int length,
+		 std::vector<double const *> const &parameters,
+		 std::vector<std::vector<unsigned int> > const &dims) const;
+    bool isSupportFixed(std::vector<bool> const &fixmask) const;
 };
 
 #endif /* DCAT_H_ */
