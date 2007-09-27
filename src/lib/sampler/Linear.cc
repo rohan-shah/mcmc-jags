389f1b36e6e847a50d79d8b3d1c8c78281f50cd2
martyn_plummer <devnull@localhost>
2007-09-27 13:53:46 +0000

    Modified linearity tests to use GraphMarks

--- src/lib/sampler/Linear.cc
+++ src/lib/sampler/Linear.cc
@@ -16,17 +16,15 @@ bool checkLinear(vector<StochasticNode*> const &snodes, Graph const &graph,
     vector<Node *> dtrm_nodes;
     Sampler::classifyChildren(snodes, graph, stoch_nodes, dtrm_nodes);
 
-    set<Node const *> parameters;
-    Graph pgraph;
+    GraphMarks linear_marks(graph);
     for (unsigned int i = 0; i < snodes.size(); ++i) {
-	parameters.insert(snodes[i]);
-        pgraph.add(snodes[i]);
+	//Stochastic nodes are trivial linear functions of themselves
+	linear_marks.mark(snodes[i], MARK_TRUE);
     }
-
+    
     for (unsigned int j = 0; j < dtrm_nodes.size(); ++j) {
-	if (dtrm_nodes[j]->isLinear(parameters, pgraph, fixed)) {	
-	    parameters.insert(dtrm_nodes[j]);
-            pgraph.add(dtrm_nodes[j]);
+	if (dtrm_nodes[j]->isLinear(linear_marks, fixed)) {	
+	    linear_marks.mark(dtrm_nodes[j], MARK_TRUE);
 	}
 	else {
 	    return false;
@@ -43,15 +41,12 @@ bool checkScale(StochasticNode* snode, Graph const &graph, bool fixed)
     Sampler::classifyChildren(vector<StochasticNode *>(1, snode), graph, 
 			      stoch_nodes, dtrm_nodes);
 
-    set<Node const *> parameters;
-    parameters.insert(snode);
-    Graph pgraph;
-    pgraph.add(snode);
+    GraphMarks scale_marks(graph);
+    scale_marks.mark(snode, MARK_TRUE);
 
     for (unsigned int j = 0; j < dtrm_nodes.size(); ++j) {
-	if (dtrm_nodes[j]->isScale(parameters, pgraph, fixed)) {	
-	    parameters.insert(dtrm_nodes[j]);
-            pgraph.add(dtrm_nodes[j]);
+	if (dtrm_nodes[j]->isScale(scale_marks, fixed)) {	
+	    scale_marks.mark(dtrm_nodes[j], MARK_TRUE);
 	}
 	else {
 	    return false;
