bf4cd842a6c9674d10183ca0b8a2731e72e7499e
martyn_plummer <devnull@localhost>
2007-10-02 10:07:37 +0000

    DMState and DExp moved out into new msm module Added namespaces to all modules

--- src/modules/bugs/functions/Max.h
+++ src/modules/bugs/functions/Max.h
@@ -3,21 +3,26 @@
 
 #include <function/Function.h>
 
-/**
- * @short Maximum element of a vector
- * @see Min
- */
-class Max: public Function
-{
-public:
-    Max ();
-    void evaluate(double *x, std::vector <double const *> const &args,
-		  std::vector<unsigned int> const &lengths,
-		  std::vector<std::vector<unsigned int> > const &dims) const;
-    bool checkParameterDim (std::vector<std::vector<unsigned int> > const &dims)
-	const;
-    /** Returns true if all arguments are true */
-    bool isDiscreteValued(std::vector<bool> const &mask) const;
-};
+namespace bugs {
+    
+    /**
+     * @short Maximum element of a vector
+     * @see Min
+     */
+    class Max: public Function
+    {
+    public:
+	Max ();
+	void evaluate(double *x, std::vector <double const *> const &args,
+		      std::vector<unsigned int> const &lengths,
+		      std::vector<std::vector<unsigned int> > const &dims) 
+	    const;
+	bool checkParameterDim (std::vector<std::vector<unsigned int> > const &dims)
+	    const;
+	/** Returns true if all arguments are true */
+	bool isDiscreteValued(std::vector<bool> const &mask) const;
+    };
+
+}
 
 #endif /* FUNC_MAX_H_ */
