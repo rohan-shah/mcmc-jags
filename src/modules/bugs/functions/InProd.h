ed0f9b06b6f7418dc5a862204f3a71839c5fd26c
martyn_plummer <devnull@localhost>
2007-10-02 10:07:29 +0000

    DMState and DExp moved out into new msm module Added namespaces to all modules

--- src/modules/bugs/functions/InProd.h
+++ src/modules/bugs/functions/InProd.h
@@ -3,27 +3,32 @@
 
 #include <function/Function.h>
 
-/**
- * @short Inner product of two vectors
- * The InProd function returns the sum of the product of corresponding
- * elements from two arrays with the same dimension
- * <pre>
- * y <- inprod(a[],b[])
- * </pre>
- */
-class InProd : public Function
-{
-public:
-    InProd ();
-    void evaluate (double *value,
-		   std::vector <double const *> const &args,
-		   std::vector<unsigned int> const &lengths,
-		   std::vector<std::vector<unsigned int> > const &dims) const;
-    bool checkParameterDim (std::vector <std::vector<unsigned int> > const &dims) const;
-    bool isDiscreteValued(std::vector<bool> const &mask) const;
-    bool isLinear(std::vector<bool> const &mask,
-		  std::vector<bool> const &fix) const;
-    bool isScale(unsigned int index, std::vector<bool> const &fix) const;
-};
+namespace bugs {
+
+    /**
+     * @short Inner product of two vectors
+     * The InProd function returns the sum of the product of corresponding
+     * elements from two arrays with the same dimension
+     * <pre>
+     * y <- inprod(a[],b[])
+     * </pre>
+     */
+    class InProd : public Function
+    {
+    public:
+	InProd ();
+	void evaluate (double *value,
+		       std::vector <double const *> const &args,
+		       std::vector<unsigned int> const &lengths,
+		       std::vector<std::vector<unsigned int> > const &dims) 
+	    const;
+	bool checkParameterDim (std::vector <std::vector<unsigned int> > const &dims) const;
+	bool isDiscreteValued(std::vector<bool> const &mask) const;
+	bool isLinear(std::vector<bool> const &mask,
+		      std::vector<bool> const &fix) const;
+	bool isScale(unsigned int index, std::vector<bool> const &fix) const;
+    };
+
+}
 
 #endif /* FUNC_INPROD_H_ */
