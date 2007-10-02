1617635c4b643aefc1d9d453530dc8069432798c
martyn_plummer <devnull@localhost>
2007-10-02 10:08:02 +0000

    DMState and DExp moved out into new msm module Added namespaces to all modules

---
+++ src/modules/msm/msm.cc
@@ -0,0 +1,39 @@
+#include <Module.h>
+#include <distribution/DistTab.h>
+
+#include <distributions/DMState.h>
+
+#include <functions/Mexp.h>
+
+using std::vector;
+
+namespace msm {
+
+    class MSMModule : public Module {
+    public:
+	MSMModule();
+	~MSMModule();
+    };
+    
+    MSMModule::MSMModule() {
+	
+	insert(new DMState);
+	insert(new Mexp);
+    }
+    
+    MSMModule::~MSMModule() {
+	
+	vector<Distribution*> const &dvec = distributions();
+	for (unsigned int i = 0; i < dvec.size(); ++i) {
+	    delete dvec[i];
+	}
+	
+	vector<Function*> const &fvec = functions();
+	for (unsigned int i = 0; i < fvec.size(); ++i) {
+	    delete fvec[i];
+	}
+    }
+}
+ 
+msm::MSMModule _msm_module;
+    
