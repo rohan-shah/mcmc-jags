23853b5120a94f2430f853ee716c885c4a8db179
martyn_plummer <devnull@localhost>
2007-10-02 10:07:51 +0000

    DMState and DExp moved out into new msm module Added namespaces to all modules

--- src/modules/bugs/functions/Sum.cc
+++ src/modules/bugs/functions/Sum.cc
@@ -4,37 +4,42 @@
 
 using std::vector;
 
-Sum::Sum () : Function("sum", 1)
-{
-}
+namespace bugs {
 
-void Sum::evaluate(double *x, vector <double const *> const &args,
-		   vector<unsigned int> const &lengths,
-		   vector<vector<unsigned int> > const &dims) const
-{
-    double value = args[0][0];
-    for (unsigned long i = 1; i < lengths[0]; ++i) {
-	value += args[0][i];
+    Sum::Sum () : Function("sum", 1)
+    {
     }
-    *x = value;
-}
 
-bool Sum::checkParameterDim (vector<vector<unsigned int> > const &args) const
-{
-    return true;
-}
+    void Sum::evaluate(double *x, vector <double const *> const &args,
+		       vector<unsigned int> const &lengths,
+		       vector<vector<unsigned int> > const &dims) const
+    {
+	double value = args[0][0];
+	for (unsigned long i = 1; i < lengths[0]; ++i) {
+	    value += args[0][i];
+	}
+	*x = value;
+    }
 
-bool Sum::isDiscreteValued(vector<bool> const &mask) const
-{
-    return allTrue(mask);
-}
+    bool 
+    Sum::checkParameterDim (vector<vector<unsigned int> > const &args) const
+    {
+	return true;
+    }
 
-bool Sum::isLinear(vector<bool> const &mask, vector<bool> const &fix) const
-{
-  return true;
-}
+    bool Sum::isDiscreteValued(vector<bool> const &mask) const
+    {
+	return allTrue(mask);
+    }
 
-bool Sum::isScale(unsigned int index, vector<bool> const &fix) const
-{
-  return true;
+    bool Sum::isLinear(vector<bool> const &mask, vector<bool> const &fix) const
+    {
+	return true;
+    }
+
+    bool Sum::isScale(unsigned int index, vector<bool> const &fix) const
+    {
+	return true;
+    }
+
 }
