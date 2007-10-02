7de88fb34a35b5ddbe3c35944c821aee36d30ae4
martyn_plummer <devnull@localhost>
2007-10-02 10:07:38 +0000

    DMState and DExp moved out into new msm module Added namespaces to all modules

--- src/modules/bugs/functions/Mean.cc
+++ src/modules/bugs/functions/Mean.cc
@@ -4,35 +4,42 @@
 
 using std::vector;
 
-Mean::Mean ()
-    : Function ("mean", 1)
-{
-}
+namespace bugs {
 
-void Mean::evaluate (double *x, vector<double const*> const &args,
-		     vector<unsigned int> const &lengths,
-                     vector<vector<unsigned int> > const &dims) const
-{
-    double svalue = 0;
-    for (unsigned int i = 0; i < lengths[0]; i++) {
-	svalue += args[0][i];
+    Mean::Mean ()
+	: Function ("mean", 1)
+    {
     }
-    svalue /= lengths[0];
-    *x = svalue;
-}
 
-bool Mean::checkParameterDim (vector<vector<unsigned int> > const &dims) const
-{
-    return true;
-}
+    void Mean::evaluate (double *x, vector<double const*> const &args,
+			 vector<unsigned int> const &lengths,
+			 vector<vector<unsigned int> > const &dims) const
+    {
+	double svalue = 0;
+	for (unsigned int i = 0; i < lengths[0]; i++) {
+	    svalue += args[0][i];
+	}
+	svalue /= lengths[0];
+	*x = svalue;
+    }
 
-bool Mean::isLinear(vector<bool> const &mask, vector<bool> const &fix) const
-{
-    return true;
-}
+    bool 
+    Mean::checkParameterDim (vector<vector<unsigned int> > const &dims) const
+    {
+	return true;
+    }
 
-bool Mean::isScale(unsigned int index, vector<bool> const &fix) const
-{
-    return true;
+    bool 
+    Mean::isLinear(vector<bool> const &mask, vector<bool> const &fix) const
+    {
+	return true;
+    }
+
+    bool 
+    Mean::isScale(unsigned int index, vector<bool> const &fix) const
+    {
+	return true;
+    }
+
 }
 
