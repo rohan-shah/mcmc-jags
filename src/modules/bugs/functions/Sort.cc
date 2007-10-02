2291176d7bad5414b2b8aa7fde8c6151422c1230
martyn_plummer <devnull@localhost>
2007-10-02 10:07:48 +0000

    DMState and DExp moved out into new msm module Added namespaces to all modules

--- src/modules/bugs/functions/Sort.cc
+++ src/modules/bugs/functions/Sort.cc
@@ -10,32 +10,38 @@ using std::vector;
 using std::sort;
 using std::logic_error;
 
-Sort::Sort ()
-  : Function ("sort", 1)
-{
-}
+namespace bugs {
 
-void Sort::evaluate (double *value, vector <double const *> const &args,
-		     vector<unsigned int> const &lengths, 
-                     vector<vector<unsigned int> > const &dims) const
-{
-  for (unsigned int i = 0; i < lengths[0]; ++i) {
-    value[i] = args[0][i];
-  }
-  sort(value, value + lengths[0]);
-}
+    Sort::Sort ()
+	: Function ("sort", 1)
+    {
+    }
 
-vector<unsigned int>  Sort::dim (vector <vector<unsigned int> > const &dims) const
-{
-    return dims[0];
-}
+    void Sort::evaluate (double *value, vector <double const *> const &args,
+			 vector<unsigned int> const &lengths, 
+			 vector<vector<unsigned int> > const &dims) const
+    {
+	for (unsigned int i = 0; i < lengths[0]; ++i) {
+	    value[i] = args[0][i];
+	}
+	sort(value, value + lengths[0]);
+    }
 
-bool Sort::checkParameterDim (vector<vector<unsigned int> > const &dims) const
-{
-    return isVector(dims[0]) || isScalar(dims[0]);
-}
+    vector<unsigned int>  
+    Sort::dim (vector <vector<unsigned int> > const &dims) const
+    {
+	return dims[0];
+    }
 
-bool Sort::isDiscreteValued(vector<bool> const &mask) const
-{
-  return count(mask.begin(), mask.end(), false) == 0;
+    bool 
+    Sort::checkParameterDim (vector<vector<unsigned int> > const &dims) const
+    {
+	return isVector(dims[0]) || isScalar(dims[0]);
+    }
+
+    bool Sort::isDiscreteValued(vector<bool> const &mask) const
+    {
+	return count(mask.begin(), mask.end(), false) == 0;
+    }
+
 }
