42084d2a298c768b29d06cac3c2cf2699627f170
martyn_plummer <devnull@localhost>
2007-10-02 10:07:44 +0000

    DMState and DExp moved out into new msm module Added namespaces to all modules

--- src/modules/bugs/functions/Prod.cc
+++ src/modules/bugs/functions/Prod.cc
@@ -4,27 +4,32 @@
 
 using std::vector;
 
-Prod::Prod () : Function("prod", 1)
-{
-}
+namespace bugs {
 
-void Prod::evaluate(double *x, vector <double const *> const &args,
-		    vector<unsigned int> const &lengths,
-		    vector<vector<unsigned int> > const &dims) const
-{
-    double value = args[0][0];
-    for (unsigned int i = 1; i < lengths[0]; ++i) {
-	value *= args[0][i];
+    Prod::Prod () : Function("prod", 1)
+    {
     }
-    *x = value;
-}
 
-bool Prod::checkParameterDim (vector<vector<unsigned int> > const &args) const
-{
-    return true;
-}
+    void Prod::evaluate(double *x, vector <double const *> const &args,
+			vector<unsigned int> const &lengths,
+			vector<vector<unsigned int> > const &dims) const
+    {
+	double value = args[0][0];
+	for (unsigned int i = 1; i < lengths[0]; ++i) {
+	    value *= args[0][i];
+	}
+	*x = value;
+    }
 
-bool Prod::isDiscreteValued(vector<bool> const &mask) const
-{
-    return allTrue(mask);
+    bool Prod::checkParameterDim (vector<vector<unsigned int> > const &args) 
+	const
+    {
+	return true;
+    }
+
+    bool Prod::isDiscreteValued(vector<bool> const &mask) const
+    {
+	return allTrue(mask);
+    }
+
 }
