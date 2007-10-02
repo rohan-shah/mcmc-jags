572249a3fa22dc8e3d1d20de3bf296b471a11e81
martyn_plummer <devnull@localhost>
2007-10-02 10:07:35 +0000

    DMState and DExp moved out into new msm module Added namespaces to all modules

--- src/modules/bugs/functions/MatMult.h
+++ src/modules/bugs/functions/MatMult.h
@@ -3,18 +3,24 @@
 
 #include <function/Function.h>
 
-class MatMult : public Function
-{
-public:
-    MatMult();
-    void evaluate (double *value, std::vector<double const *> const &args,
-		   std::vector<unsigned int> const &lengths,
-		   std::vector<std::vector<unsigned int> > const &dims) const;
-    std::vector<unsigned int> 
-	dim(std::vector<std::vector<unsigned int> > const &dims) const;
-    bool checkParameterDim(std::vector <std::vector<unsigned int> > const &dims) const;
-    bool isLinear(std::vector<bool> const &mask, std::vector<bool> const &fix) const;
+namespace bugs {
+
+    class MatMult : public Function
+    {
+    public:
+	MatMult();
+	void evaluate(double *value, std::vector<double const *> const &args,
+		      std::vector<unsigned int> const &lengths,
+		      std::vector<std::vector<unsigned int> > const &dims) 
+	    const;
+	std::vector<unsigned int> 
+	    dim(std::vector<std::vector<unsigned int> > const &dims) const;
+	bool checkParameterDim(std::vector <std::vector<unsigned int> > const &dims) const;
+	bool isLinear(std::vector<bool> const &mask, 
+		      std::vector<bool> const &fix) const;
 	
-};
+    };
+
+}
 
 #endif /* MAT_MULT_H_ */
