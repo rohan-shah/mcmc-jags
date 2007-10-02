21c9840aa26d48753bf3a11fd55210d9a55136d5
martyn_plummer <devnull@localhost>
2007-10-02 10:08:10 +0000

    DMState and DExp moved out into new msm module Added namespaces to all modules

---
+++ src/modules/msm/functions/Mexp.h
@@ -0,0 +1,22 @@
+#ifndef MEXP_H_
+#define MEXP_H_
+
+#include <function/Function.h>
+
+namespace msm {
+
+class Mexp : public Function
+{
+public:
+    Mexp();
+    void evaluate (double *value, std::vector<double const *> const &args,
+		   std::vector<unsigned int> const &lengths,
+		   std::vector<std::vector<unsigned int> > const &dims) const;
+    std::vector<unsigned int> 
+	dim(std::vector<std::vector<unsigned int> > const &dims) const;
+    bool checkParameterDim(std::vector <std::vector<unsigned int> > const &dims) const;
+};
+
+}
+
+#endif /* MEXP_H_ */
