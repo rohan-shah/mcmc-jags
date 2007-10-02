34470ee566aa85190efb090ee85fe4b38a99c6c2
martyn_plummer <devnull@localhost>
2007-10-02 10:07:51 +0000

    DMState and DExp moved out into new msm module Added namespaces to all modules

--- src/modules/bugs/functions/Sum.h
+++ src/modules/bugs/functions/Sum.h
@@ -2,27 +2,33 @@
 #define FUNC_SUM_H_
 
 #include <function/Function.h>
-/**
- * @short Sum of an array
- * Sum calculates the sum of the elements of an array
- * @see Mean
- * <pre>
- * y <- sum(x[])
- * </pre>
- */
-class Sum : public Function
-{
-public:
-  Sum ();
-  void evaluate (double *x, std::vector <double const *> const &args,
-		 std::vector<unsigned int> const &lengths,
-		 std::vector<std::vector<unsigned int> > const &dims) const;
-  bool checkParameterDim (std::vector<std::vector<unsigned int> > const &dims)
-      const;
-  bool isDiscreteValued(std::vector<bool> const &mask) const;
-  bool isLinear(std::vector<bool> const &mask,
-                std::vector<bool> const &isfixed) const;
-  bool isScale(unsigned int index, std::vector<bool> const &mask) const;
-};
+
+namespace bugs {
+
+    /**
+     * @short Sum of an array
+     * Sum calculates the sum of the elements of an array
+     * @see Mean
+     * <pre>
+     * y <- sum(x[])
+     * </pre>
+     */
+    class Sum : public Function
+    {
+    public:
+	Sum ();
+	void evaluate(double *x, std::vector <double const *> const &args,
+		      std::vector<unsigned int> const &lengths,
+		      std::vector<std::vector<unsigned int> > const &dims) 
+	    const;
+	bool checkParameterDim(std::vector<std::vector<unsigned int> > const &dims)
+	    const;
+	bool isDiscreteValued(std::vector<bool> const &mask) const;
+	bool isLinear(std::vector<bool> const &mask,
+		      std::vector<bool> const &isfixed) const;
+	bool isScale(unsigned int index, std::vector<bool> const &mask) const;
+    };
+    
+}
 
 #endif /* FUNC_SUM_H_ */
