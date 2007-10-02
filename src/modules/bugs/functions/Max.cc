0870a5fa167269b340d7a16db4f710af2ffa244d
martyn_plummer <devnull@localhost>
2007-10-02 10:07:36 +0000

    DMState and DExp moved out into new msm module Added namespaces to all modules

--- src/modules/bugs/functions/Max.cc
+++ src/modules/bugs/functions/Max.cc
@@ -8,30 +8,34 @@ using std::vector;
 using std::max_element;
 using std::max;
 
-Max::Max ()
-  : Function("max", 0)
-{
-}
+namespace bugs {
 
-void Max::evaluate(double *value,  vector<double const *> const &args,
-		   vector<unsigned int> const &lengths,
-		   vector<vector<unsigned int> > const &dims) const
-{
-    double ans = *max_element(args[0], args[0] + lengths[0]);
-    for (unsigned int i = 1; i < args.size(); ++i) {
-	double maxi = *max_element(args[i], args[i] + lengths[i]);
-	ans = max(ans, maxi);
+    Max::Max ()
+	: Function("max", 0)
+    {
     }
-    *value = ans;
-}
 
-bool Max::checkParameterDim (vector<vector<unsigned int> > const &dims) const
-{
-  return true;
-}
+    void Max::evaluate(double *value,  vector<double const *> const &args,
+		       vector<unsigned int> const &lengths,
+		       vector<vector<unsigned int> > const &dims) const
+    {
+	double ans = *max_element(args[0], args[0] + lengths[0]);
+	for (unsigned int i = 1; i < args.size(); ++i) {
+	    double maxi = *max_element(args[i], args[i] + lengths[i]);
+	    ans = max(ans, maxi);
+	}
+	*value = ans;
+    }
 
-bool Max::isDiscreteValued(vector<bool> const &mask) const
-{
-    return allTrue(mask);
-}
+    bool 
+    Max::checkParameterDim (vector<vector<unsigned int> > const &dims) const
+    {
+	return true;
+    }
 
+    bool Max::isDiscreteValued(vector<bool> const &mask) const
+    {
+	return allTrue(mask);
+    }
+
+}
