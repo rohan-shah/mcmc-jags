e7c082e9c9142bb8ee86580edfc61085bba388db
martyn_plummer <devnull@localhost>
2007-10-02 10:07:52 +0000

    DMState and DExp moved out into new msm module Added namespaces to all modules

--- src/modules/bugs/functions/Transpose.cc
+++ src/modules/bugs/functions/Transpose.cc
@@ -5,34 +5,39 @@
 
 using std::vector;
 
-Transpose::Transpose()
-    : Function("t",1)
-{
-}
+namespace bugs {
 
-void
-Transpose::evaluate (double *value, vector<double const *> const &args,
-		     vector<unsigned int> const &lengths,
-                     vector<vector<unsigned int> > const &dims) const
-{
-    unsigned int nrow = dims[0][0];
-    unsigned int ncol = dims[0].size() == 2 ? dims[0][1] : 1;
-    for (unsigned int i = 0; i < lengths[0]; ++i) {
-	value[i] = args[0][(i / ncol) + (i % ncol) * nrow];
+    Transpose::Transpose()
+	: Function("t",1)
+    {
     }
-}
 
-vector<unsigned int> 
-Transpose::dim (vector <vector<unsigned int> > const &dims) const
-{
-    vector<unsigned int> ans(2);
-    ans[0] = dims[0].size() == 2 ? dims[0][1] : 1;
-    ans[1] = dims[0][0];
-    return ans;
-}
+    void
+    Transpose::evaluate (double *value, vector<double const *> const &args,
+			 vector<unsigned int> const &lengths,
+			 vector<vector<unsigned int> > const &dims) const
+    {
+	unsigned int nrow = dims[0][0];
+	unsigned int ncol = dims[0].size() == 2 ? dims[0][1] : 1;
+	for (unsigned int i = 0; i < lengths[0]; ++i) {
+	    value[i] = args[0][(i / ncol) + (i % ncol) * nrow];
+	}
+    }
 
-bool 
-Transpose::checkParameterDim (vector <vector<unsigned int> > const &dims) const
-{
-    return dims[0].size() == 1 || dims[0].size() == 2;
+    vector<unsigned int> 
+    Transpose::dim (vector <vector<unsigned int> > const &dims) const
+    {
+	vector<unsigned int> ans(2);
+	ans[0] = dims[0].size() == 2 ? dims[0][1] : 1;
+	ans[1] = dims[0][0];
+	return ans;
+    }
+
+    bool 
+    Transpose::checkParameterDim (vector <vector<unsigned int> > const &dims) 
+	const
+    {
+	return dims[0].size() == 1 || dims[0].size() == 2;
+    }
+
 }
