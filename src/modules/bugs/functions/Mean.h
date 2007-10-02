a9c95832caf754852fa22f9d5f745ab1c0a9aa60
martyn_plummer <devnull@localhost>
2007-10-02 10:07:38 +0000

    DMState and DExp moved out into new msm module Added namespaces to all modules

--- src/modules/bugs/functions/Mean.h
+++ src/modules/bugs/functions/Mean.h
@@ -3,25 +3,29 @@
 
 #include <function/Function.h>
 
-/**
- * @short Mean function
- * Mean calculates the mean of the elements of an array
- * @see SD
- * <pre>
- * y <- mean(x[])
- * </pre>
- */
-class Mean : public Function
-{
-public:
-    Mean ();
-    void evaluate(double *x, std::vector<double const *> const &args,
-		  std::vector<unsigned int> const &lengths,
-		  std::vector<std::vector<unsigned int> > const &dims) const;
-    bool checkParameterDim (std::vector<std::vector<unsigned int> > const &dims) const;
-    bool isLinear(std::vector<bool> const &mask,
-		  std::vector<bool> const &fix) const;
-    bool isScale(unsigned int index, std::vector<bool> const &fix) const;
-};
+namespace bugs {
+
+    /**
+     * @short Mean function
+     * Mean calculates the mean of the elements of an array
+     * @see SD
+     * <pre>
+     * y <- mean(x[])
+     * </pre>
+     */
+    class Mean : public Function
+    {
+    public:
+	Mean ();
+	void evaluate(double *x, std::vector<double const *> const &args,
+		      std::vector<unsigned int> const &lengths,
+		      std::vector<std::vector<unsigned int> > const &dims) 
+	    const;
+	bool checkParameterDim (std::vector<std::vector<unsigned int> > const &dims) const;
+	bool isLinear(std::vector<bool> const &mask,
+		      std::vector<bool> const &fix) const;
+	bool isScale(unsigned int index, std::vector<bool> const &fix) const;
+    };
+}
 
 #endif /* FUNC_MEAN_H_ */
