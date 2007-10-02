96c91bff0ac1e6f3d5dcbb581e9aab9864ad5683
martyn_plummer <devnull@localhost>
2007-10-02 10:07:31 +0000

    DMState and DExp moved out into new msm module Added namespaces to all modules

--- src/modules/bugs/functions/LogDet.cc
+++ src/modules/bugs/functions/LogDet.cc
@@ -7,21 +7,26 @@
 
 using std::vector;
 
-LogDet::LogDet ()
-  : Function ("logdet", 1)
-{
-}
+namespace bugs {
 
-void LogDet::evaluate (double *x, vector<double const *> const &args,
-		       vector<unsigned int> const &lengths,
-                       vector<vector<unsigned int> > const &dims) const
-{
-  *x = logdet(args[0], dims[0][0]);
-}
+    LogDet::LogDet ()
+	: Function ("logdet", 1)
+    {
+    }
 
-bool LogDet::checkParameterDim (vector<vector<unsigned int> > const &dims) const
-{
-  return isSquareMatrix(dims[0]);
+    void LogDet::evaluate (double *x, vector<double const *> const &args,
+			   vector<unsigned int> const &lengths,
+			   vector<vector<unsigned int> > const &dims) const
+    {
+	*x = logdet(args[0], dims[0][0]);
+    }
+
+    bool 
+    LogDet::checkParameterDim (vector<vector<unsigned int> > const &dims) const
+    {
+	return isSquareMatrix(dims[0]);
+    }
+
 }
 
 /* FIXME: we need a checkParameterValue function */
