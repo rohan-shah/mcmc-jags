b4ae87414aeda9a310803227004a71edcb486eff
martyn_plummer <devnull@localhost>
2007-10-02 10:07:44 +0000

    DMState and DExp moved out into new msm module Added namespaces to all modules

--- src/modules/bugs/functions/Prod.h
+++ src/modules/bugs/functions/Prod.h
@@ -3,24 +3,29 @@
 
 #include <function/Function.h>
 
-/**
- * @short Product of an array
- * Sum calculates the product of the elements of an array
- * @see Sum
- * <pre>
- * y <- prod(x[])
- * </pre>
- */
-class Prod : public Function
-{
-public:
-  Prod ();
-  void evaluate (double *x, std::vector <double const *> const &args,
-		 std::vector<unsigned int> const &lengths,
-		 std::vector<std::vector<unsigned int> > const &dims) const;
-  bool checkParameterDim (std::vector<std::vector<unsigned int> > const &dims)
-      const;
-  bool isDiscreteValued(std::vector<bool> const &mask) const;
-};
+namespace bugs {
+
+    /**
+     * @short Product of an array
+     * Sum calculates the product of the elements of an array
+     * @see Sum
+     * <pre>
+     * y <- prod(x[])
+     * </pre>
+     */
+    class Prod : public Function
+    {
+    public:
+	Prod ();
+	void evaluate(double *x, std::vector <double const *> const &args,
+		      std::vector<unsigned int> const &lengths,
+		      std::vector<std::vector<unsigned int> > const &dims)
+	    const;
+	bool checkParameterDim(std::vector<std::vector<unsigned int> > const &dims)
+	    const;
+	bool isDiscreteValued(std::vector<bool> const &mask) const;
+    };
+
+}
 
 #endif /* FUNC_PROD_H_ */
