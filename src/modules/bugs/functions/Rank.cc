54ec3d182c3d88988f7cd5c58fdad52320cf20e7
martyn_plummer <devnull@localhost>
2007-10-02 10:07:45 +0000

    DMState and DExp moved out into new msm module Added namespaces to all modules

--- src/modules/bugs/functions/Rank.cc
+++ src/modules/bugs/functions/Rank.cc
@@ -11,42 +11,46 @@ bool lt_doubleptr (double const *arg1, double const *arg2) {
   return *arg1 < *arg2;
 }
 
-Rank::Rank ()
-  : Function ("rank", 1)
-{
-}
+namespace bugs {
 
-void Rank::evaluate(double *value, vector<double const *> const &args,
-		    vector<unsigned int> const &lengths,
-                    vector<vector<unsigned int> > const &dims) const
-{
-  long len = product(dims[0]);
+    Rank::Rank ()
+	: Function ("rank", 1)
+    {
+    }
 
-  //Create a vector of pointers to the elements of arg and sort it
-  double const **argptrs = new double const *[len];
-  for (long i = 0; i < len; ++i) {
-    argptrs[i] = args[0] + i;
-  }
-  stable_sort(argptrs, argptrs + len, lt_doubleptr);
+    void Rank::evaluate(double *value, vector<double const *> const &args,
+			vector<unsigned int> const &lengths,
+			vector<vector<unsigned int> > const &dims) const
+    {
+	long len = product(dims[0]);
 
-  //Ranks can be inferred from the sorted vector of pointers
-  for (long i = 0; i < len; ++i) {
-    value[argptrs[i] - args[0]] = i + 1;
-  }
-  delete [] argptrs;
-}
+	//Create a vector of pointers to the elements of arg and sort it
+	double const **argptrs = new double const *[len];
+	for (long i = 0; i < len; ++i) {
+	    argptrs[i] = args[0] + i;
+	}
+	stable_sort(argptrs, argptrs + len, lt_doubleptr);
 
-vector<unsigned int>  Rank::dim (vector <vector<unsigned int> > const &dims) const
-{
-  return dims[0];
-}
+	//Ranks can be inferred from the sorted vector of pointers
+	for (long i = 0; i < len; ++i) {
+	    value[argptrs[i] - args[0]] = i + 1;
+	}
+	delete [] argptrs;
+    }
 
-bool Rank::checkParameterDim (vector<vector<unsigned int> > const &dims) const
-{
-    return isVector(dims[0]) || isScalar(dims[0]);
-}
+    vector<unsigned int>  Rank::dim (vector <vector<unsigned int> > const &dims) const
+    {
+	return dims[0];
+    }
 
-bool Rank::isDiscreteValued(vector<bool> const &mask) const
-{
-  return true;
+    bool Rank::checkParameterDim (vector<vector<unsigned int> > const &dims) const
+    {
+	return isVector(dims[0]) || isScalar(dims[0]);
+    }
+
+    bool Rank::isDiscreteValued(vector<bool> const &mask) const
+    {
+	return true;
+    }
+
 }
