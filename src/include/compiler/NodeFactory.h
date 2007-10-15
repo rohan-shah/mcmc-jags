a57b081395c4d52d65de030055b75874b12802f9
martyn_plummer <devnull@localhost>
2007-10-15 17:11:53 +0000

    Modified SArray class to use STL vectors

--- src/include/compiler/NodeFactory.h
+++ src/include/compiler/NodeFactory.h
@@ -11,6 +11,7 @@ inline bool lt(double value1, double value2)
     return value1 < value2 - 16 * DBL_EPSILON;
 }
 
+bool lt(std::vector<double> const &value1, std::vector<double> const &value2);
 bool lt(double const *value1, double const *value2, unsigned int length);
 bool lt(Node const *node1, Node const *node2);
 bool lt(std::vector<Node const *> const &par1, 
