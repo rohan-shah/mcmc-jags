903cb685316842de368a0201b28655e89e56eaa7
martyn_plummer <devnull@localhost>
2007-11-16 16:11:17 +0000

    Support for windows installer

--- src/terminal/jags.bat
+++ src/terminal/jags.bat
@@ -1,6 +1,6 @@
 setlocal
-set JAGS_HOME=c:\Program Files\jags
+set JAGS_HOME=@JAGS_HOME@
 set PATH=%JAGS_HOME%\bin;%JAGS_HOME%\lib;%PATH%
 set LTDL_LIBRARY_PATH=%JAGS_HOME%\modules
-jags.bin %1
+jags-terminal %1
 
