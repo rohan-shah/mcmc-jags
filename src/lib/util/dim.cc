fa71c6cf22500d7282b2990527a9a9d171ca0b58
martyn_plummer <devnull@localhost>
2008-01-01 17:39:55 +0000

    Use references for dimensions (and vectors of dimensions) to save memory

--- src/lib/util/dim.cc
+++ src/lib/util/dim.cc
@@ -1,7 +1,35 @@
 #include <config.h>
 #include <util/dim.h>
 
+#include <set>
+
 using std::vector;
+using std::set;
+
+vector<unsigned int> const &getUnique(vector<unsigned int> const &dim)
+{
+    static set<vector<unsigned int> > _dimset;
+    set<vector<unsigned int> >::const_iterator p = _dimset.find(dim);
+    if (p == _dimset.end()) {
+	_dimset.insert(dim);
+	p = _dimset.find(dim);
+    }
+    return *p;
+}
+
+vector<vector<unsigned int> > const &
+getUnique(vector<vector<unsigned int> > const &dimvec)
+{
+    static set<vector<vector<unsigned int> > > _dimvecset;
+    set<vector<vector<unsigned int> > >::const_iterator p = 
+       _dimvecset.find(dimvec);
+    if (p == _dimvecset.end()) {
+	_dimvecset.insert(dimvec);
+	p = _dimvecset.find(dimvec);
+    }
+    return *p;
+    
+}
 
 unsigned int product(vector<unsigned int> const &x)
 {
