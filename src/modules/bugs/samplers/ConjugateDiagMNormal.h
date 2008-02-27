2827aa4bd0b7245780aab09a42e79abdec86721a
martyn_plummer <devnull@localhost>
2008-02-27 23:40:53 +0000

    Specialist conjugate sampler

---
+++ src/modules/bugs/samplers/ConjugateDiagMNormal.h
@@ -0,0 +1,26 @@
+#ifndef CONJUGATE_DIAG_M_NORMAL_H_
+#define CONJUGATE_DIAG_M_NORMAL_H_
+
+#include "ConjugateMethod.h"
+
+class Graph;
+
+/**
+ * Special conjugate sampler for multivariate normal distribution
+ * in the case where all stochastic children are scalars with a 
+ * normal distribution, and for which the coefficient matrix is
+ * diagonal.  The usual conjugate normal sampler scales badly in
+ * this situation
+ */
+class ConjugateDiagMNormal : public ConjugateMethod {
+    vector<unsigned int> _offsets;
+public:
+    ConjugateDiagMNormal();
+    ~ConjugateDiagMNormal();
+    void initialize(ConjugateSampler *sampler, Graph const &graph);
+    void update(ConjugateSampler *sampler, unsigned int chain, RNG *rng) const;
+    static bool canSample(StochasticNode *snode, Graph const &graph);
+    std::string name() const;
+};
+
+#endif /* CONJUGATE_DIAG_M_NORMAL_H_ */
