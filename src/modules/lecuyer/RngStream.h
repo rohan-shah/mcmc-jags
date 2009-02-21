e8e027d063b414ef252500e8f2dc3d6efafe91bc
martyn_plummer <devnull@localhost>
2009-02-21 14:37:48 +0000

    Simplified RngStream interface.  Some code moved from RngStream.c to RngstreamFactory.cc

--- src/modules/lecuyer/RngStream.h
+++ src/modules/lecuyer/RngStream.h
@@ -3,30 +3,34 @@
 
 #include <rng/RmathRNG.h>
 
+#define m1    4294967087.0
+#define m2    4294944443.0
+
 namespace lecuyer {
     
+    /**
+     * @short MRG32k3a combined multiple recursive generator
+     */
     class RngStream : public RmathRNG {
-	bool Anti;
-	int IncPrec;
-	double Cg[6], Bg[6], Ig[6];
-	double U01();
-	double U01d();
+	double Cg[6];
     public:
-	RngStream(unsigned int seed);
-    
-	void resetStartStream ();
-	void resetNextStream ();
-	void resetStartSubstream ();
-	void resetNextSubstream ();
-	void setAntithetic (bool a);
-	void increasedPrecis (bool incp);
-	void advanceState (long e, long c);
-
+	/**
+	 * Constructor for RngStream random number generator
+	 *
+	 * @param state Initial state for the RNG. The first three
+	 * elements must be less than m1, and the second three must be
+	 * less than m2. The first three elements may not be all zero
+	 * and the second elements may not be all zero.
+	 */
+	RngStream(unsigned int state[6]);
 	void init(unsigned int seed);
 	bool setState(std::vector<int> const &state);
 	void getState(std::vector<int> &state) const;
 	double uniform();
-    
+	/**
+	 * Generates a state vector from a random seed
+	 */
+	static void seedToState(unsigned int seed, unsigned int state[6]);
     };
  
 }
