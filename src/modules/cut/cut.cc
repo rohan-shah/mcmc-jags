54dc8090fc1c9cbe4d13ef6d37b8e2e85158de20
martyn_plummer <devnull@localhost>
2009-07-06 22:18:34 +0000

    First check in of cut module

---
+++ src/modules/cut/cut.cc
@@ -0,0 +1,38 @@
+#include <Module.h>
+//#include <distribution/DistTab.h>
+
+#include "DCut.h"
+#include "CutSamplerFactory.h"
+
+using std::vector;
+using namespace cut;
+
+class CutModule : public Module {
+public:
+    CutModule();
+    ~CutModule();
+};
+
+CutModule::CutModule() {
+
+    //Load distributions
+    insert(new DCut);
+
+    //Load sampler factories
+    insert(new CutSamplerFactory);
+}
+
+CutModule::~CutModule() {
+
+    vector<Distribution*> const &dvec = distributions();
+    for (unsigned int i = 0; i < dvec.size(); ++i) {
+	delete dvec[i];
+    }
+
+    vector<SamplerFactory*> const &svec = samplerFactories();
+    for (unsigned int i = 0; i < svec.size(); ++i) {
+	delete svec[i];
+    }
+}
+
+CutModule _cut_module;
