441e66574f824d8bc612db58a768364fa0c53fc4
martyn_plummer <devnull@localhost>
2009-07-07 16:33:24 +0000

    First stab at CutSampler

---
+++ src/modules/cut/CutSampler.cc
@@ -0,0 +1,88 @@
+#include <config.h>
+
+using std::vector;
+using std::set;
+
+static vector<StochasticNode*>
+appendChildren(vector<StochasticNode*> nodes, Graph const &graph)
+{
+    set<StochasticNode*> children;
+    Sampler::getStochasticChildren(nodes, graph, children);
+    for (set<StochasticNode*>::const_iterator p = children.begin();
+	 p != children.end(); ++p)
+    {
+	nodes.push_back(*p);
+    }
+    return nodes;
+}
+
+namespace cut {
+
+    CutSampler::CutSampler(vector<StochasticNode*> const &cutnodes,
+			   Graph const &graph)
+	: Sampler(appendChildren(cutnodes), graph), _ncut(cutnodes.size())
+    {
+	for (unsigned int ch = 0; ch < cutnodes[0]->nchain(); ++ch) {
+	    _methods.push_back(new CutMethod(this));
+	}
+    }
+
+    CutSampler::~CutSampler()
+    {
+	for (unsigned int ch = 0; ch < _methods.size(); ++ch) {
+	    delete _methods[ch];
+	}
+    }
+
+    unsigned int CutSampler::ncut() const
+    {
+	return _ncut;
+    }
+
+    void CutSampler::update(std::vector<RNG*> const &rngs)
+    {
+	for (unsigned int ch = 0; ch < rngs.size(); ++ch) {
+	    _methods[ch]->update(rngs[ch]);
+	}
+    }
+
+    bool CutSampler::adaptOff()
+    {
+	bool pass = true;
+	for (unsigned int ch = 0; ch < _methods.size(); ++ch) {
+	    if (!_methods[ch]->adaptOff())
+		pass = false;
+	}
+	return pass;
+    }
+
+    bool CutSampler::isAdaptive() const
+    {
+	return true;
+    }
+
+    string CutSampler::name() const
+    {
+	return "CutSampler";
+    }
+
+    bool CutSampler::cansample(StochasticNode *cutnode, Graph const &graph)
+    {
+	//Check that stochastic children within the graph are all continuous
+	//with fixed support
+
+	set<StochasticNode*> children;
+	Sampler::getStochasticChildren(vector<StochasticNode*>(1, node), 
+				       graph, children);
+	for (set<StochasticNode*>::const_iterator p = children.begin();
+	     p != children.end(); ++p)
+	{
+	    if ((*p)->isDiscreteValued())
+		return false;
+	    if (!(*p)->isSupportFixed())
+		return false;
+	}
+	return true;
+    }
+
+}
---
+++ src/modules/cut/CutSampler.h
@@ -0,0 +1,29 @@
+#ifndef CUT_SAMPLER_H_
+#define CUT_SAMPLER_H_
+
+#include <sampler/Sampler.h>
+
+class CutMethod;
+
+namespace cut {
+
+    class CutSampler : public Sampler
+    {
+	std::vector<StochasticNode*> _cutnodes;
+	unsigned int _ncut;
+	std::vector<CutMethod*> _methods;
+    public:
+	CutSampler(std::vector<StochasticNode*> const &cutnodes,
+		   Graph const &graph);
+	~CutSampler();
+	unsigned int ncut();
+	void update(std::vector<RNG*> const &);
+	bool adaptOff();
+	bool isAdaptive() const;
+	std::string name() const;
+	static bool cansample(StochasticNode* cutnode, Graph const &graph);
+    };
+    
+}
+
+#endif /* CUT_SAMPLER_H_ */
