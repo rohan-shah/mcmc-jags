886cf2b406c2f7e3cbeaa228dd428eb98d2c7177
martyn_plummer <devnull@localhost>
2009-09-25 13:38:17 +0000

    Major overhaul of the Sampler object system. - Member functions of the sampler class have been hived off into a new   concrete class called Updater. - Sampler objects use an updater to interfact with the graphical model - Update methods are written in an abstract way, completely independent   of the implementation of the graphical model. The connection is only   made by the sub-classes instantiated in the modules. - Updater class encapsulates local information in the graph and could   be used as an interface for many other functions. Current update has been tested with classic bugs examples, but may be a source of future bugs.

--- src/lib/sampler/Metropolis.cc
+++ src/lib/sampler/Metropolis.cc
@@ -1,7 +1,5 @@
 #include <config.h>
 #include <sampler/Metropolis.h>
-#include <sampler/DensitySampler.h>
-#include <graph/StochasticNode.h>
 #include <rng/RNG.h>
 
 #include <algorithm>
@@ -12,78 +10,13 @@ using std::vector;
 using std::copy;
 using std::min;
 
-static unsigned int addDF(vector<StochasticNode *> const &nodes)
+Metropolis::Metropolis(vector<double> const &value)
+    : _last_value(value), _adapt(true)
 {
-    unsigned int d = 0;
-    for (unsigned int i = 0; i < nodes.size(); ++i) {
-        d += df(nodes[i]);
-     }
-     return d;
-}
-
-static unsigned int addLength(vector<StochasticNode *> const &nodes)
-{
-    unsigned int d = 0;
-    for (unsigned int i = 0; i < nodes.size(); ++i) {
-	d += nodes[i]->length();
-    }
-    return d;
-}
-
-Metropolis::Metropolis(vector<StochasticNode*> const &nodes)
-    : _adapt(true), _value(0), _last_value(0), _length(0)
-{
-    _length = addLength(nodes);
-    _value = new double[_length];
-    _last_value = new double[_length];
 }
 
 Metropolis::~Metropolis()
 {
-    delete [] _value;
-    delete [] _last_value;
-}
-
-void Metropolis::initialize(DensitySampler *sampler, unsigned int chain)
-{
-    /* 
-       We can't do this in the constructor because we are relying
-       on virtual functions transform and untransform. These are
-       defined by a subclass of Metropolis which does not yet
-       exist when the constructor is called
-    */
-
-    unsigned int n = addLength(sampler->nodes());
-    double *node_values = new double[n];
-
-    vector<StochasticNode *> const &nodes = sampler->nodes();
-    double *nvp = node_values;
-    for (unsigned int i = 0; i < nodes.size(); ++i) {
-	double const *nvalue = nodes[i]->value(chain);
-	unsigned int nlength = nodes[i]->length();
-	copy(nvalue, nvalue + nlength, nvp);
-	nvp += nlength;
-    }
-    untransform(node_values, n, _value, _length);
-
-    delete [] node_values;
-
-    copy(_value, _value + _length, _last_value);
-
-    initMetropolis(sampler, chain);
-}
-
-void Metropolis::propose(double const *value, unsigned int Nvalue)
-{
-    if (Nvalue != _length) {
-	throw logic_error("Invalid length in Metropolis::propose");
-    }
-    copy(value, value + Nvalue, _value);
-    unsigned int Nnode = _sampler->length();
-    double *node_values = new double[Nnode];
-    transform(value, Nvalue, node_values, Nnode);
-    _sampler->setValue(node_values, Nnode, _chain);
-    delete [] node_values;
 }
 
 bool Metropolis::accept(RNG *rng, double prob)
@@ -91,11 +24,11 @@ bool Metropolis::accept(RNG *rng, double prob)
     bool accept = rng->uniform() <= prob;
     if (accept) {
 	//Store current value as last accepted value
-	copy(_value, _value + _length, _last_value);
+	getValue(_last_value);
     }
     else {
 	//Revert to last accepted value
-	propose(_last_value, _length);
+	setValue(_last_value);
     }
     if (_adapt) {
 	rescale(min(prob, 1.0));
@@ -114,18 +47,8 @@ bool Metropolis::isAdaptive() const
     return true;
 }
 
-unsigned int Metropolis::value_length() const
+unsigned int Metropolis::length() const
 {
-    return _length;
+    return _last_value.size();
 }
 
-double const *Metropolis::value() const
-{
-    return _value;
-}
-
-void Metropolis::initMetropolis(DensitySampler *sampler, unsigned int chain)
-{
-    //This function only exists for a subclass to override instead of
-    //Metropolis::initialize
-}
