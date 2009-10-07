eb58c28603cc20e7727864c4a1c8457eb0c3eba2
martyn_plummer <devnull@localhost>
2009-10-07 22:24:51 +0000

    Removed debugging messages

--- src/modules/glm/samplers/ProbitFactory.cc
+++ src/modules/glm/samplers/ProbitFactory.cc
@@ -11,20 +11,15 @@
 
 using std::string;
 
-//debuggin
-#include <iostream>
-
 namespace glm {
 
     bool ProbitFactory::checkOutcome(StochasticNode const *snode) const
     {
 	string const &dname = snode->distribution()->name();
 	if (dname == "dbern") {
-	    std::cout << "dbern" << std::endl;
 	    return true;
 	}
 	else if (dname == "dbin") {
-	    std::cout << "dbin" << std::cout;
 	    Node const *N = snode->parents()[1];
 	    if (N->length() != 1)
 		return false;
@@ -35,19 +30,12 @@ namespace glm {
 	    return true;
 	}
 	else {
-	    std::cout << "Something else (" << dname << ")" << std::endl;
 	    return false;
 	}
     }
     
     bool ProbitFactory::checkLink(InverseLinkFunc const *link) const
     {
-	if (link->linkName() == "probit") {
-	    std::cout << "probit!" << std::endl;
-	}
-	else {
-	    std::cout << "Not probit (" << link->linkName() << ")" << std::endl;
-	}
 	return link->linkName() == "probit";
     }
 
