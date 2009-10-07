b87ee378ff5f9b03e9f6bd996c3a9122782ef27b
martyn_plummer <devnull@localhost>
2009-10-07 16:09:01 +0000

    First stab at Holmes-Held probit sampler

---
+++ src/modules/glm/samplers/Probit.cc
@@ -0,0 +1,142 @@
+#include <config.h>
+
+#include "Probit.h"
+#include "TruncatedNormal.h"
+#include <graph/StochasticNode.h>
+#include <sampler/Updater.h>
+#include <rng/RNG.h>
+
+#include <stdexcept>
+
+using std::vector;
+using std::string;
+using std::logic_error;
+
+static unsigned int nchildren(Updater const *updater)
+{
+    return updater->stochasticChildren().size();
+}
+
+namespace glm {
+
+    Probit::Probit(Updater const *updater, unsigned int chain)
+	: GLMMethod(updater, chain, true), _z(nchildren(updater))
+    {
+    }
+
+    double Probit::getMean(unsigned int i) const
+    {
+	Node const *child = _updater->stochasticChildren()[i];
+	return child->parents()[0]->parents()[0]->value(_chain)[0];
+    }
+    
+    double 
+    Probit::getPrecision(unsigned int i) const 
+    {
+	return 1;
+    }
+
+    double Probit::getValue(unsigned int i) const 
+    {
+	return _z[i];
+    }
+    
+    string Probit::name() const
+    {
+	return "Probit";
+    }
+    
+    void Probit::initAuxiliary(RNG *rng)
+    {
+	vector<StochasticNode const *> const &schildren = 
+	    _updater->stochasticChildren();
+
+	for (unsigned int r = 0; r < _z.size(); ++r) {
+	    double yr = schildren[r]->value(_chain)[0];
+	    if (yr == 1) {
+		_z[r] = LNormal(getMean(r), 1, 0, rng);
+	    }
+	    else if (yr == 0) {
+		_z[r] = RNormal(getMean(r), 1, 0, rng);
+	    }
+	    else {
+		throw logic_error("Invalid child value in Probit");
+	    }
+	}
+	
+    }
+    
+    void Probit::updateAuxiliary(double *b, csn const *N, RNG *rng)
+    {
+	vector<StochasticNode const *> const &schildren = 
+	    _updater->stochasticChildren();
+
+	int nrow = schildren.size();
+	int ncol = _updater->length();
+
+	//Create transpose of design matrix for easier access to rows
+	cs *t_X = cs_transpose(_X, 1);
+	int *Tp = t_X->p;
+	int *Ti = t_X->i;
+	double *Tx = t_X->x;
+
+	double *xr = new double[ncol]; // Row r of design matrix
+	double *S = new double[ncol];  // V %*% xr
+	double *w = new double[ncol];  // work space
+	for (unsigned int r = 0; r < nrow; ++r) {
+
+	    //Copy row i of design matrix to array xr;
+	    for (unsigned int i = 0; i < ncol; ++i) {
+		xr[i] = 0;
+	    }
+	    for (unsigned int c = Tp[r]; c < Tp[r+1]; ++c) {
+		xr[Ti[c]] = Tx[c];
+	    }
+	
+	    //Subtract contribution of row r to b
+	    for (unsigned int i = 0; i < ncol; ++i) {
+		b[i] -= _z[r] * xr[i];
+	    }
+
+	    //Calculate mean and variance of z[r] conditional
+	    //on z[s] for s != r
+
+	    cs_ipvec(_symbol->pinv, xr, w, ncol);
+	    cs_lsolve(N->L, w);
+	    cs_ltsolve(N->L, w);
+	    cs_pvec(_symbol->pinv, w, S, nrow);
+	    
+	    double hr = 0; // Hat[r,r]
+	    double zr_mean = 0;
+	    for (unsigned int i = 0; i < ncol; ++i) {
+		hr += S[i] * xr[i];
+		zr_mean +=  S[i] * b[i];
+	    }
+	    zr_mean /= (1 - hr);
+	    double zr_var = 1/(1 - hr);
+
+	    double yr = schildren[r]->value(_chain)[0];
+	    if (yr == 1) {
+		_z[r] = LNormal(zr_mean, sqrt(zr_var), 0, rng);
+	    }
+	    else if (yr == 0) {
+		_z[r] = RNormal(zr_mean, sqrt(zr_var), 0, rng);
+	    }
+	    else {
+		throw logic_error("Invalid child value in Probit");
+	    }
+
+	    //Add new contribution of row r back to b
+	    for (unsigned int i = 0; i < ncol; ++i) {
+		b[i] -= _z[r] * xr[i];
+	    }
+	}
+
+	//Free workspace
+	delete [] w;
+	delete [] S;
+	delete [] xr;
+	cs_spfree(t_X);
+
+    }
+}
---
+++ src/modules/glm/samplers/Probit.h
@@ -0,0 +1,25 @@
+#ifndef PROBIT_H_
+#define PROBIT_H_
+
+#include "GLMMethod.h"
+
+namespace glm {
+
+    /**
+     * Conjugate sampler for normal linear models.
+     */
+    class Probit : public GLMMethod {
+	std::vector<double> _z;
+    public:
+	Probit(Updater const *updater, unsigned int chain);
+	double getMean(unsigned int i) const;
+	double getPrecision(unsigned int i) const;
+	double getValue(unsigned int i) const;
+	void initAuxiliary(RNG *rng);
+	void updateAuxiliary(double *b, csn const *N, RNG *rng);
+	std::string name() const;
+    };
+    
+}
+
+#endif /* PROBIT_H_ */
