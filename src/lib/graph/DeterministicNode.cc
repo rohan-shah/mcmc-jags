7530f05f030598eba28d20fab8dc442ba2307b6f
martyn_plummer <devnull@localhost>
2009-12-23 15:22:19 +0000

    Node::initializeData is obsolete. We now initialize data in DeterministicNodes when they are constructed.  This has to be done in the constructor for the sub-class that defines deterministicSample.

--- src/lib/graph/DeterministicNode.cc
+++ src/lib/graph/DeterministicNode.cc
@@ -5,11 +5,30 @@ using std::vector;
 
 DeterministicNode::DeterministicNode(vector<unsigned int> const &dim, 
                                      vector<Node const *> const &parents)
-  : Node(dim, parents)
+    : Node(dim, parents), _observed(true)
 {
+    //Add this node as a deterministic child of its parents
     for (unsigned int i = 0; i < parents.size(); ++i) {
 	parents[i]->addChild(this);
     }
+
+    //Deterministic nodes are not observed if any parents are unobserved
+    vector<Node const*>::const_iterator p;
+    for (p = parents.begin(); p != parents.end(); ++p)
+    {
+	if (!(*p)->isObserved()) {
+	    _observed =  false;
+	    break;
+	}
+    }
+
+    /* 
+       Observed deterministic nodes should be immediately initialized
+       by calling deterministicSample. We can't do this here because
+       this is a virtual function. So we have to do it in whatever
+       sub-class defines the deterministicSample member function.
+    */
+       
 }
 
 DeterministicNode::~DeterministicNode()
@@ -29,12 +48,6 @@ bool DeterministicNode::isRandomVariable() const {
 
 bool DeterministicNode::isObserved() const
 {
-    vector<Node const*>::const_iterator p;
-    for (p = parents().begin(); p != parents().end(); ++p)
-    {
-	if (!(*p)->isObserved())
-	    return false;
-    }
-    return true;
+    return _observed;
 }
 
