a4ce29c68f7d7710f51d2b9c958899c68d5b82d3
martyn_plummer <devnull@localhost>
2010-01-06 16:06:09 +0000

    Tidy up conjugate samplers

--- src/modules/bugs/samplers/ConjugateBeta.h
+++ src/modules/bugs/samplers/ConjugateBeta.h
@@ -15,7 +15,7 @@
 class ConjugateBeta : public ConjugateMethod {
 public:
     ConjugateBeta(GraphView const *gv);
-    void update(GraphView *gv, unsigned int chain, RNG *rng) const;
+    void update(unsigned int chain, RNG *rng) const;
     static bool canSample(StochasticNode *snode, Graph const &graph);
     std::string name() const;
 };
