5ec07ffd96b79ca9d1b82e4cc5dc5473c0677a5e
martyn_plummer <devnull@localhost>
2010-01-14 16:39:26 +0000

    Major overhaul of the Function class, which now has four separate sub-classes (InverseLinkFunc, ScalarFunction, VectorFunction, and ArrayFunction). The compiler will create corresponding sub-classes of the LogicalNode class (LinkNode, ScalarLogicalNode, VectorLogicalNode, and ArrayLogicalNode).  For easy handling of these function classes, a polymorphic Function pointer class FunctionPtr has been created.
    In addition, the module code for registering observable functions has
    been moved out of distTab and into a new class obsFuncTab.

--- src/include/distribution/DistTab.h
+++ src/include/distribution/DistTab.h
@@ -6,7 +6,6 @@
 #include <map>
 
 class Distribution;
-class Function;
 
 /** 
  * @short Look-up table for Distribution objects
@@ -20,7 +19,6 @@ class Function;
  */
 class DistTab {
     std::list<Distribution const *> _dist_list;
-    std::map<Distribution const *, Function const *> _func_map;
 public:
   /**
    * Inserts a distribution into the table.  The same distribution cannot
@@ -36,17 +34,6 @@ public:
    */
   void insert(Distribution const *dist);
   /**
-   * Inserts an observable function into the table. Observable
-   * functions may behave like either a distribution or a function,
-   * depending on the context, so must be represented by both a
-   * Distribution object and a Function object. Both objects must have
-   * the same name in the BUGS language.
-   *
-   * @param dist Pointer to the distribution
-   * @param func Pointer to the function
-   */
-  void insert(Distribution const *dist, Function const *func);
-  /**
    * Finds a distribution by name. If two distributions with the
    * same name have been inserted into the table, the most recently
    * inserted one is returned.
@@ -54,13 +41,6 @@ public:
    * @return a pointer to the distribution or 0 if it was not found.
    */
   Distribution const *find(std::string const &name) const;
-  /**
-   * Finds an Function correspoding to Distribution when both objects
-   * represent the same observable function.
-   *
-   * @return a pointer to the function or 0 if it was not found.
-   */
-  Function const *findFunction(Distribution const *dist) const;
   /**
    * Removes a distribution from the table. A DistTab does no memory
    * management and will not delete the erased distribution.  This
