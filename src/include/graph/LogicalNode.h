34199f9bbb059d1d89650d2d4a7714ade77f7b11
martyn_plummer <devnull@localhost>
2010-01-14 16:39:27 +0000

    Major overhaul of the Function class, which now has four separate sub-classes (InverseLinkFunc, ScalarFunction, VectorFunction, and ArrayFunction). The compiler will create corresponding sub-classes of the LogicalNode class (LinkNode, ScalarLogicalNode, VectorLogicalNode, and ArrayLogicalNode).  For easy handling of these function classes, a polymorphic Function pointer class FunctionPtr has been created.
    In addition, the module code for registering observable functions has
    been moved out of distTab and into a new class obsFuncTab.

--- src/include/graph/LogicalNode.h
+++ src/include/graph/LogicalNode.h
@@ -15,31 +15,21 @@ class Function;
  */
 class LogicalNode : public DeterministicNode {
     Function const * const _func;
-    std::vector<std::vector<double const*> > _parameters;
-    std::vector<std::vector<unsigned int> > const &_dims;
-    std::vector<unsigned int> const &_lengths;
-
+protected:
+    const std::vector<std::vector<double const*> > _parameters;
 public:
     /**
      * A logical node is defined by a function (which may be an inline
      * operator in the BUGS language) and its parameters.
      */
-    LogicalNode(Function const *func,
-                std::vector<Node const*> const &parameters);
-    ~LogicalNode();
+    LogicalNode(std::vector<unsigned int> const &dim, 
+                std::vector<Node const*> const &parameters,
+		Function const *func);
     /**
      * A LogicalNode may be discrete valued if its parents are. 
      * @see Function##isDiscreteValued
      */
     bool isDiscreteValued() const;
-    /**
-     * Calculates the value of the node based on the parameters. 
-     */
-    void deterministicSample(unsigned int chain);
-    /**
-     * @see Function#checkParameterValue.
-     */
-    bool checkParentValues(unsigned int chain) const;
     /**
      * A LogicalNode belongs to a closed class if its function
      * preserves that class.
@@ -49,7 +39,6 @@ public:
     bool isClosed(std::set<Node const *> const &ancestors, 
 		  ClosedFuncClass fc, bool fixed) const;
     std::string deparse(std::vector<std::string> const &) const;
-    Node *clone(std::vector<Node const *> const &parents) const;
 };
 
 #endif /* LOGICAL_NODE_H_ */
