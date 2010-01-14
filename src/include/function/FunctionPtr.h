9748c324f44ae7a421007402077e6a897b7c61c7
martyn_plummer <devnull@localhost>
2010-01-14 22:05:43 +0000

    InverseLinkFunc renamed to LinkFunction

--- src/include/function/FunctionPtr.h
+++ src/include/function/FunctionPtr.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTION_POINTER_H_
 #define FUNCTION_POINTER_H_
 
-#include <function/InverseLinkFunc.h>
+#include <function/LinkFunction.h>
 #include <function/ScalarFunction.h>
 #include <function/VectorFunction.h>
 #include <function/ArrayFunction.h>
@@ -10,13 +10,13 @@
  * @short Polymorphic pointer to Function
  *
  * A FunctionPtr holds a pointer to one of the four sub-classes of
- * Function: InverseLinkFunc, ScalarFunction, VectorFunction, or
+ * Function: LinkFunction, ScalarFunction, VectorFunction, or
  * ArrayFunction.  The pointers can be extracted using the extractor
  * functions LINK, SCALAR, VECTOR, and ARRAY, respectively . 
  */
 class FunctionPtr
 {
-    InverseLinkFunc const * lfunc;
+    LinkFunction const * lfunc;
     ScalarFunction const * sfunc;
     VectorFunction const * vfunc;
     ArrayFunction const * afunc;
@@ -25,10 +25,10 @@ public:
     FunctionPtr(ScalarFunction const *);
     FunctionPtr(VectorFunction const *);
     FunctionPtr(ArrayFunction const *);
-    FunctionPtr(InverseLinkFunc const *);
+    FunctionPtr(LinkFunction const *);
     bool operator==(FunctionPtr const &rhs) const;
     std::string const &name() const;
-    friend InverseLinkFunc const *LINK(FunctionPtr const &p);
+    friend LinkFunction const *LINK(FunctionPtr const &p);
     friend ScalarFunction const *SCALAR(FunctionPtr const &p);
     friend VectorFunction const *VECTOR(FunctionPtr const &p);
     friend ArrayFunction const *ARRAY(FunctionPtr const &p);
