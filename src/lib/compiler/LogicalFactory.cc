f3a153b2fdc86c268685c0776a3e33404ac3735f
martyn_plummer <devnull@localhost>
2010-01-14 16:39:27 +0000

    Major overhaul of the Function class, which now has four separate sub-classes (InverseLinkFunc, ScalarFunction, VectorFunction, and ArrayFunction). The compiler will create corresponding sub-classes of the LogicalNode class (LinkNode, ScalarLogicalNode, VectorLogicalNode, and ArrayLogicalNode).  For easy handling of these function classes, a polymorphic Function pointer class FunctionPtr has been created.
    In addition, the module code for registering observable functions has
    been moved out of distTab and into a new class obsFuncTab.

--- src/lib/compiler/LogicalFactory.cc
+++ src/lib/compiler/LogicalFactory.cc
@@ -6,26 +6,43 @@
 #include <graph/LogicalNode.h>
 #include <graph/Node.h>
 #include <graph/ScalarLogicalNode.h>
+#include <graph/VectorLogicalNode.h>
+#include <graph/ArrayLogicalNode.h>
+#include <graph/VSLogicalNode.h>
 #include <graph/LinkNode.h>
-#include <function/InverseLinkFunc.h>
-#include <function/ScalarFunc.h>
-#include <function/Function.h>
 #include <util/dim.h>
 
 #include <stdexcept>
+#include <string>
 
 using std::pair;
 using std::map;
 using std::vector;
 using std::invalid_argument;
+using std::runtime_error;
+using std::string;
+
+static bool lt(FunctionPtr const &arg1, FunctionPtr const &arg2)
+{
+    if (SCALAR(arg1) < SCALAR(arg2))
+	return true;
+    else if (LINK(arg1) < LINK(arg2))
+	return true;
+    else if (VECTOR(arg1) < VECTOR(arg2))
+	return true;
+    else if (ARRAY(arg1) < ARRAY(arg2))
+	return true;
+    else
+	return false;
+}
 
 bool lt(LogicalPair const &arg1, LogicalPair const &arg2)
 {
     // Order first by function
-    if (arg1.first < arg2.first) {
+    if (lt(arg1.first,arg2.first)) {
 	return true;
     }
-    else if (arg1.first > arg2.first) {
+    else if (lt(arg1.first,arg2.first)) {
 	return false;
     }
     else {
@@ -34,45 +51,66 @@ bool lt(LogicalPair const &arg1, LogicalPair const &arg2)
     }
 }
 
-Node* LogicalFactory::getLinkNode(InverseLinkFunc const *link, 
-				  vector<Node const *> const &parents,
-				  Model &model)
+LogicalNode* LogicalFactory::newNode(FunctionPtr const &func, 
+				     vector<Node const *> const &parents)
 {
-    if (link == 0) {
-	throw invalid_argument("NULL function passed to getLogicalNode");
-    }
-    
-    LogicalPair lpair(link, parents);
-    map<LogicalPair, Node*, ltlogical>::iterator i = _logicalmap.find(lpair);
-    
-    if (i != _logicalmap.end()) {
-	return i->second;
-    }
-    else {
-	// Create a node
-
-	DeterministicNode *lnode = 0;
-	if (isScalar(parents[0]->dim())) {
-	    lnode = new LinkNode(link, parents);
+    LogicalNode *node = 0;
+    if (SCALAR(func)) {
+	unsigned int arglength = 1;
+	for (unsigned int i = 0; i < parents.size(); ++i) {
+	    if (parents[i]->length() > 1) {
+		if (arglength == 1) {
+		    arglength = parents[i]->length();
+		}
+		else if (parents[i]->length() != arglength) {
+		    arglength = 0;
+		    break;
+		}
+	    }
+	}
+	if (arglength == 0) {
+	    throw runtime_error(string("Incompatible argument lengths for ") +
+				func.name());
+	}
+	else if (arglength == 1) {
+	    node = new ScalarLogicalNode(SCALAR(func), parents);
 	}
 	else {
-	    //Vectorized
-	    lnode = new LogicalNode(link, parents);
+	    node = new VSLogicalNode(SCALAR(func), parents);
 	}
-	_logicalmap[lpair] = lnode;
-	model.addNode(lnode);
-	return lnode;
     }
+    else if (LINK(func)) {
+	bool ok = true;
+	for (unsigned int i = 0; i < parents.size(); ++i) {
+	    if (parents[i]->length() != 1) {
+		ok = false;
+		break;
+	    }
+	}
+	if (ok) {
+	    node = new LinkNode(LINK(func), parents);
+	}
+	else {
+	    throw runtime_error("Invalid vector argument to " +	func.name());
+	}
+    }
+    else if (VECTOR(func))
+	node = new VectorLogicalNode(VECTOR(func), parents);
+    else if (ARRAY(func))
+	node = new ArrayLogicalNode(ARRAY(func), parents);
+    else
+	throw invalid_argument("Invalid function in getNode");
+    return node;
 }
 
-Node* LogicalFactory::getNode(Function const *func, 
+Node* LogicalFactory::getNode(FunctionPtr const &func, 
 			      vector<Node const *> const &parents,
 			      Model &model)
 {
-    if (func == 0) {
+    if (isNULL(func)) {
 	throw invalid_argument("NULL function passed to getLogicalNode");
     }
-
+    
     LogicalPair lpair(func, parents);
     map<LogicalPair, Node*, ltlogical>::iterator i = _logicalmap.find(lpair);
 
@@ -80,28 +118,7 @@ Node* LogicalFactory::getNode(Function const *func,
 	return i->second;
     }
     else {
-	// Create a node
-
-	//FIXME: Downcasting to an incomplete type
-
-	ScalarFunc const * sfunc = dynamic_cast<ScalarFunc const *>(func);
-	bool vectorized = false;
-	if (sfunc) {
-	    for (unsigned int i = 0; i < parents.size(); ++i) {
-		if (!isScalar(parents[i]->dim())) {
-		    vectorized = true;
-		    break;
-		}
-	    }
-	}
-
-	DeterministicNode *lnode = 0;
-	if (sfunc && !vectorized) {
-	    lnode = new ScalarLogicalNode(sfunc, parents);
-	}
-	else {
-	    lnode = new LogicalNode(func, parents);
-	}
+	LogicalNode *lnode = newNode(func, parents);
 	_logicalmap[lpair] = lnode;
 	model.addNode(lnode);
 	return lnode;
