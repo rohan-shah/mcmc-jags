7977d80666a6d121a9280097d909435e55cdf490
martyn_plummer <devnull@localhost>
2010-01-14 20:09:19 +0000

    Renamed from ScalarFunc.cc

---
+++ src/lib/function/ScalarFunction.cc
@@ -0,0 +1,35 @@
+#include <config.h>
+#include <function/ScalarFunction.h>
+#include <util/dim.h>
+
+#include <algorithm>
+
+using std::vector;
+using std::string;
+using std::find_if;
+
+ScalarFunction::ScalarFunction (string const &name, unsigned int npar)
+  : Function (name, npar)
+{
+}
+
+bool 
+ScalarFunction::checkParameterValue(vector<double const *> const &args) const
+{
+    return true;
+}
+
+bool ScalarFunction::isPower(std::vector<bool> const &mask,
+			     std::vector<bool> const &isfixed) const
+{
+    unsigned int nmask = 0;
+    for (unsigned int i = 0; i < mask.size(); ++i) {
+	nmask += mask[i];
+    }
+    
+    if (nmask > 1)
+	return false;
+    else
+	return isScale(mask, vector<bool>());
+    
+}
