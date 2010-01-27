ce4ca9927c16709939676c137644288b373e5e93
martyn_plummer <devnull@localhost>
2010-01-27 11:20:14 +0000

    Expanded trig functions and added hyperbolic functions to match OpenBUGS

---
+++ src/modules/bugs/functions/ArcTanh.cc
@@ -0,0 +1,27 @@
+#include <config.h>
+#include "ArcTanh.h"
+
+#include <cmath>
+
+using std::vector;
+
+namespace bugs {
+
+    ArcTanh::ArcTanh ()
+	: ScalarFunction ("arctanh", 1)
+    {
+    }
+
+    double ArcTanh::evaluate(vector<double const *> const &args) const
+    {
+	double x = *args[0];
+	return (log(1 + x) - log(1 - x))/2;
+    }
+
+        
+    bool ArcTanh::checkParameterValue(vector<double const *> const &args) const
+    {
+	return *args[0] > -1 && *args[0] <  1;
+    }
+
+}
