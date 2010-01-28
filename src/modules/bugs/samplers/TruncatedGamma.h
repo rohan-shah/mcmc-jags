bb129a2b927eb9553a05585a6c83ece051f8166e
martyn_plummer <devnull@localhost>
2010-01-28 17:15:07 +0000

    Added TruncatedGamma sampler. Its principal use is for a uniform distribution on the standard deviation, but it works more generally. This is the first use of the isScale member function in the Function class.

---
+++ src/modules/bugs/samplers/TruncatedGamma.h
@@ -0,0 +1,19 @@
+#ifndef TRUNCATED_GAMMA_H_
+#define TRUNCATED_GAMMA_H_
+
+#include "ConjugateMethod.h"
+
+class Node;
+
+class TruncatedGamma : public ConjugateMethod {
+    double _shape;
+    Node const *_parent;
+public:
+    TruncatedGamma(GraphView const *gv);
+    ~TruncatedGamma();
+    static bool canSample(StochasticNode *snode, Graph const &graph);
+    void update(unsigned int chain, RNG *rng) const;
+    std::string name() const;
+};
+
+#endif /* TRUNCATED_GAMMA_H_ */
