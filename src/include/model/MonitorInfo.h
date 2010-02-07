0f54c360e57da79be6136193ab907f43a3a4c5f0
martyn_plummer <devnull@localhost>
2010-02-07 16:08:21 +0000

    Changes to the Monitor sub-system to make it more flexible.

---
+++ src/include/model/MonitorInfo.h
@@ -0,0 +1,32 @@
+#ifndef MONITOR_INFO_H_
+#define MONITOR_INFO_H_
+
+#include <string>
+#include <sarray/Range.h>
+
+class Monitor;
+
+/**
+ * @short Info a monitor 
+ */
+class MonitorInfo {
+    Monitor * _monitor;
+    std::string _name;
+    Range _range;
+    std::string _type;
+public:
+    /** 
+     * Constructor
+     * @param monitor Monitor 
+     
+     */
+    MonitorInfo(Monitor *monitor, std::string const &name,
+		Range const &range, std::string const &type);
+    Monitor *monitor() const;
+    std::string const &name() const;
+    std::string const &type() const;
+    Range const &range() const;
+    bool operator==(MonitorInfo const &rhs) const;
+};
+
+#endif /* MONITOR_INFO_H_ */
