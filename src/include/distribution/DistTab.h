ddd9369d1384538de5b9507fa03d3f8248157729
martyn_plummer <devnull@localhost>
2010-03-23 23:25:26 +0000

    Major update to distributions - now in separate subclasses for scalar, vector, and array-valued distributions. Corresponding sub-classes introduced for StochasticNode. No more downcasting to StochasticNode from Node.

--- src/include/distribution/DistTab.h
+++ src/include/distribution/DistTab.h
@@ -3,56 +3,37 @@
 
 #include <list>
 #include <string>
-#include <map>
 
-class Distribution;
+#include <distribution/DistPtr.h>
 
-/** 
+/**
  * @short Look-up table for Distribution objects
  *
- * Since all member functions of the Distribution class are constant,
- * only one instance of any Distribution object is required. The
- * DistTab class provides a means of storing Distribution objects and
- * looking them up by name.
+ * The DistTab class provides a means of looking up Distributions by
+ * their name in the BUGS language.
  *
- * @see FuncTab
+ * @see Dist DistTab 
  */
-class DistTab {
-    std::list<Distribution const *> _dist_list;
+class DistTab
+{
+    std::list<DistPtr> _flist;
+    DistPtr const _nullfun;
 public:
-  /**
-   * Inserts a distribution into the table.  The same distribution cannot
-   * be inserted into the table twice. If this is attempted, the second
-   * call to insert will have no effect. Conversely, two different 
-   * distributions with the same name in the BUGS language can be inserted.
-   * In this case, the second distribution is always returned by 
-   * DistTab#find.
-   *
-   * @param dist Pointer to the distribution to insert. The pointer
-   * must be valid for the lifetime of the DistTab, or until the
-   * distribution is removed with a call to DistTab#erase.
-   */
-  void insert(Distribution const *dist);
-  /**
-   * Finds a distribution by name. If two distributions with the
-   * same name have been inserted into the table, the most recently
-   * inserted one is returned.
-   *
-   * @return a pointer to the distribution or 0 if it was not found.
-   */
-  Distribution const *find(std::string const &name) const;
-  /**
-   * Removes a distribution from the table. A DistTab does no memory
-   * management and will not delete the erased distribution.  This
-   * must be done by the owner of the distribution. Conversely only
-   * the owner can call the erase function since it requires a non-constant
-   * pointer.
-   */
-  void erase(Distribution *dist);
-  /**
-   * Returns the list of Distributions inserted into the DistTab.
-   */
-  std::list<Distribution const *> const &distributions() const;
+    /**
+     * Inserts a dist into the table. 
+     */
+    void insert (DistPtr const &dist);
+    /**
+     * Finds a distribution by name.
+     *
+     * @return a polymorphic dist pointer. If the distribution cannot
+     * be found, then the pointer is a null DistPtr object.
+     */
+    DistPtr const &find(std::string const &name) const;
+    /**
+     * Removes a distribution from the table. 
+     */
+    void erase(DistPtr const &dist);
 };
 
 #endif /* DIST_TAB_H_ */
