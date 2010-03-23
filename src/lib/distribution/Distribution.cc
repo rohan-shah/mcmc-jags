c090beebb9b5dfb601e1f32d2abd82a712a68587
martyn_plummer <devnull@localhost>
2010-03-23 23:26:16 +0000

    Major update to distributions - now in separate subclasses for scalar, vector, and array-valued distributions. Corresponding sub-classes introduced for StochasticNode. No more downcasting to StochasticNode from Node.

--- src/lib/distribution/Distribution.cc
+++ src/lib/distribution/Distribution.cc
@@ -5,9 +5,8 @@
 using std::vector;
 using std::string;
 
-Distribution::Distribution(string const &name, unsigned int npar, 
-			   bool canbound, bool discrete)
-    : _name(name), _npar(npar), _canbound(canbound), _discrete(discrete)
+Distribution::Distribution(string const &name, unsigned int npar)
+    : _name(name), _npar(npar)
 {
 }
 
@@ -20,38 +19,10 @@ string const &Distribution::name() const
   return _name;
 }
 
-bool Distribution::isDiscreteValued() const
-{
-  return _discrete;
-}
-
 unsigned int Distribution::npar() const
 {
   return _npar;
 }
-
-bool Distribution::canBound() const
-{
-  return _canbound;
-}
-
-bool Distribution::checkParameterValue(vector<double const *> const &args,
-           vector<vector<unsigned int> > const &dims) const
-{
-  return true;
-}
-
-vector<unsigned int>
-Distribution::dim(vector<vector<unsigned int> > const &dims) const
-{
-    return vector<unsigned int>(1,1);
-}
-
-unsigned int Distribution::df(vector<vector<unsigned int> > const &pdims) const
-{
-    return product(dim(pdims));
-}
-
 
 bool Distribution::isLocationParameter(unsigned int i) const
 {
@@ -59,6 +30,16 @@ bool Distribution::isLocationParameter(unsigned int i) const
 }
 
 bool Distribution::isScaleParameter(unsigned int i) const
+{
+    return false;
+}
+
+bool Distribution::isDiscreteValued() const
+{
+    return false;
+}
+
+bool Distribution::canBound() const
 {
     return false;
 }
