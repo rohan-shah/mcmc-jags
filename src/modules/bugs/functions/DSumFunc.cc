02093798d4e1400cfd15d4f3eb83bcedcf274b95
martyn_plummer <devnull@localhost>
2010-04-06 16:18:40 +0000

    - Reinstated lost check for discrete parameters when making Stochastic node - Removed redundant checks of the number of parameters in subclasses of StochasticNode - Allowed dsum to take arbitrary number of parameters.

--- src/modules/bugs/functions/DSumFunc.cc
+++ src/modules/bugs/functions/DSumFunc.cc
@@ -7,15 +7,18 @@ using std::vector;
 namespace bugs {
 
     DSumFunc::DSumFunc()
-	: ScalarFunction("dsum", 2)
+	: ScalarFunction("dsum", 0)
     {
     }
     
     double DSumFunc::evaluate(vector<double const *> const &args) const
     {
-	return *args[0] + *args[1];
+	double ans = 0;
+	for (unsigned int i = 0; i < args.size(); ++i) {
+	    ans += *args[i];
+	}
+	return ans;
     }
-
     
     bool DSumFunc::isDiscreteValued(vector<bool> const &mask) const
     {
