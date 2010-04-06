cb50ed8c27325c946001b379d3ec90ac9cae8f8b
martyn_plummer <devnull@localhost>
2010-04-06 12:55:49 +0000

    Modified Distribution::isDiscreteValued to take arguments DSum can be discrete- or real-valued, depending on arguments Allow DSumMethod to work with discrete or real values

--- src/modules/bugs/distributions/DHyper.h
+++ src/modules/bugs/distributions/DHyper.h
@@ -29,7 +29,6 @@ class DHyper : public RScalarDist {
   bool checkParameterValue(std::vector<double const*> const &parameters) const;
   bool isSupportFixed(std::vector<bool> const &fixmask) const;
   bool canBound() const;
-  bool isDiscreteValued() const;
 };
 
 #endif /* DHYPER_H_ */
