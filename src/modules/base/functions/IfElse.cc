be3107639068121d3f3b563711a9793e9bf6a96f
martyn_plummer <devnull@localhost>
2010-05-14 12:25:06 +0000

    Added switch function

---
+++ src/modules/base/functions/IfElse.cc
@@ -0,0 +1,22 @@
+#include <config.h>
+#include "IfElse.h"
+
+using std::vector;
+
+namespace base {
+
+    IfElse::IfElse () : ScalarFunction("ifelse", 3)
+    {
+    }
+    
+    double IfElse::evaluate(vector<double const *> const &args) const
+    {
+	return *args[0] ? *args[1] : *args[2];
+    }
+    
+    bool IfElse::isDiscreteValued(vector<bool> const &mask) const
+    {
+	return mask[1] && mask[2];
+    }
+
+}
---
+++ src/modules/base/functions/IfElse.h
@@ -0,0 +1,29 @@
+#ifndef IF_ELSE_H_
+#define IF_ELSE_H_
+
+#include <function/ScalarFunction.h>
+
+namespace base {
+
+/**
+ * The ifelse function returns either a or b depending on whether the 
+ * value of x is zero or not
+ * 
+ * @short Conditional selection 
+ * <pre>
+ * y <- ifelse(x,a,b)
+ * y = a if x != 0 
+ * y = b if x == 0
+ * </pre>
+ */
+class IfElse : public ScalarFunction
+{
+public:
+    IfElse ();
+    double evaluate(std::vector<double const *> const &args) const;
+    bool isDiscreteValued(std::vector<bool> const &mask) const;
+};
+
+}
+
+#endif /* IF_ELSE_H_ */
