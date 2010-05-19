d5a8bb36ab927b5e4da8e3e6d8605c49b287c4bf
martyn_plummer <devnull@localhost>
2010-05-19 12:37:26 +0000

    Separate inverse functions for symmetric positive-definite and general matrices.

--- src/modules/bugs/matrix/matrix.h
+++ src/modules/bugs/matrix/matrix.h
@@ -1,7 +1,8 @@
 #ifndef MATRIX_H_
 #define MATRIX_H_
 
-void inverse (double *X, double const *A, int n, bool spd);
+void inverse (double *X, double const *A, int n);
+void inverse_spd (double *X, double const *A, int n);
 double logdet(double const *a, int n);
 
 #endif /* MATRIX_H_ */
