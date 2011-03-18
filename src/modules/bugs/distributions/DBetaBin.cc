5ef6406621090bd5879bb5729d0f09224474f349
martyn <devnull@localhost>
2011-03-18 21:21:39 +0100

    Added missing files for Beta-Binomial distribution

---
+++ src/modules/bugs/distributions/DBetaBin.cc
@@ -0,0 +1,113 @@
+#include <config.h>
+#include "DBetaBin.h"
+
+#include <algorithm>
+
+#include <JRmath.h>
+
+using std::vector;
+using std::max;
+using std::min;
+using std::string;
+
+#define A(par) (*par[0])
+#define B(par) (*par[1])
+#define SIZE(par) (*par[2])
+
+static double dbb(double x, double a, double b, double n)
+{
+    //Probability density function
+    return choose(a+x-1, x) * choose(b+n-x-1, n-x) / choose(a+b+n-1, n);
+}
+
+DBetaBin::DBetaBin()
+    : RScalarDist("dbetabin", 3, DIST_SPECIAL, true)
+{}
+
+string DBetaBin::alias() const
+{
+    return "dbetabinom";
+}
+
+bool 
+DBetaBin::checkParameterDiscrete (vector<bool> const &mask) const
+{
+    return mask[1] == true;
+}
+
+bool DBetaBin::checkParameterValue (vector<double const *> const &par) const
+{
+    return (SIZE(par) >= 0 && A(par) > 0.0 && B(par) > 0.0);
+}
+
+double DBetaBin::d(double x, vector<double const *> const &par, 
+		   bool give_log) const
+{
+    double y = dbb(x, A(par), B(par), SIZE(par));
+    return give_log ? log(y) : y;
+}
+
+static double pbb(double x, double a, double b, double n)
+{
+    //Distribution function
+
+    if (x < 0) return 0;
+    if (x >= n) return 1;
+    
+    double y = 0;
+    for (int i = 0; i <= x; ++i) {
+	y += dbb(i, a, b, n);
+    }
+    return y;
+}
+
+double DBetaBin::p(double x, vector<double const *> const &par, 
+		   bool lower, bool give_log) const
+{
+    double y = pbb(x, A(par), B(par), SIZE(par));
+    if (!lower) y = 1 - y;
+    return give_log ? log(y) : y;
+}
+
+static double qbb(double p, double a, double b, double n)
+{
+    //Quantile function
+    
+    if (p < 0) return 0;
+    if (p >= 1) return n;
+
+    double pi = 0;
+    for (int i = 0; i < n; ++i) {
+	pi += dbb(i, a, b, n);
+	if (pi > p) return i;
+    }
+    return n;
+}
+
+double DBetaBin::q(double p, vector<double const *> const &par, 
+		   bool lower, bool log_p) const
+{
+    if (log_p) p = exp(p);
+    if (!lower) p = 1 - p;
+    return qbb(p, A(par), B(par), SIZE(par));
+}
+
+double DBetaBin::r(vector<double const *> const &par, RNG *rng) const
+{
+    return rbinom(SIZE(par), rbeta(A(par), B(par), rng), rng);
+}
+
+double DBetaBin::l(vector<double const *> const &par) const
+{
+    return 0;
+}
+
+double DBetaBin::u(vector<double const *> const &par) const
+{
+    return SIZE(par);
+}
+
+bool DBetaBin::isSupportFixed(vector<bool> const &fixmask) const
+{
+   return fixmask[2]; //SIZE is fixed;
+} 
---
+++ src/modules/bugs/distributions/DBetaBin.h
@@ -0,0 +1,39 @@
+#ifndef DBETA_BIN_H_
+#define DBETA_BIN_H_
+
+#include "RScalarDist.h"
+
+/**
+ * @short Beta-Binomial distribution
+ * <pre>
+ * Y ~ dbin(p, n, phi)
+ * f(y|p,n,phi) = choose(a+y-1, y) * choose(b+n-y-1, n-y) / choose(a+b+n-1, n)
+ * a = p * (phi - 1)
+ * b = (1 - p) * (phi - 1)
+ * </pre>
+ */
+class DBetaBin : public RScalarDist {
+ public:
+  DBetaBin();
+  std::string alias() const;
+  double d(double x, std::vector<double const *> const &parameters, 
+	   bool give_log) const;
+  double p(double x, std::vector<double const *> const &parameters, bool lower,
+	   bool give_log) const;
+  double q(double p, std::vector<double const *> const &parameters, bool lower,
+	   bool log_p) const;
+  double r(std::vector<double const *> const &parameters, RNG *rng) const;
+  double l(std::vector<double const *> const &parameters) const;
+  double u(std::vector<double const *> const &parameters) const;
+  /**
+   * Checks that n is discrete-valued.
+   */
+  bool checkParameterDiscrete (std::vector<bool> const &mask) const;
+  /**
+   * Checks that p lies in (0,1),  n >= 1, phi > -1
+   */
+  bool checkParameterValue(std::vector<double const *> const &parameters) const;
+  bool isSupportFixed(std::vector<bool> const &fixmask) const;
+};
+
+#endif /* DBETA_BIN_H_ */
