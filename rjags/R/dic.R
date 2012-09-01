cac6bf9b38ef8d58969485fb04f583f72cd6cb0d
martyn <devnull@localhost>
2012-09-01 15:31:28 +0200

    Incorporate rjags into main repository.

---
+++ rjags/R/dic.R
@@ -0,0 +1,73 @@
+"dic.samples" <-
+  function(model, n.iter, thin=1, type="pD", ...)
+{
+    if (nchain(model) == 1) {
+        stop("2 or more parallel chains required")
+    }
+    if (!inherits(model, "jags"))
+      stop("Invalid JAGS model")
+    
+    if (!is.numeric(n.iter) || length(n.iter) != 1 || n.iter <= 0)
+      stop("n.iter must be a positive integer")
+    load.module("dic", quiet=TRUE)
+    limits <- vector("list",2)
+    pdtype <- match.arg(type, c("pD","popt"))
+    ok <- .Call("set_monitors", model$ptr(), c("deviance",pdtype),
+                limits, limits, as.integer(thin), "mean", PACKAGE="rjags")
+    if (!isTRUE(ok)) {
+      stop("Failed to set monitors")
+    }
+    
+    update(model, n.iter = as.integer(n.iter), ...)
+    dev <- .Call("get_monitored_values_flat", model$ptr(), "mean",
+                 PACKAGE="rjags")
+    for (i in seq(along=dev)) {
+        class(dev[[i]]) <- "mcarray"
+    }
+    
+    .Call("clear_monitor", model$ptr(), "deviance", NULL, NULL, "mean",
+          PACKAGE="rjags")
+    .Call("clear_monitor", model$ptr(), pdtype, NULL, NULL, "mean",
+          PACKAGE="rjags")
+
+    ans <- list("deviance" = dev$deviance, "penalty" = dev[[type]],
+                "type" = type)
+    class(ans) <- "dic"
+    return(ans)
+}
+
+"print.dic" <- function(x, digits= max(3, getOption("digits") - 3), ...)
+{
+    deviance <- sum(x$deviance)
+    cat("Mean deviance: ", format(deviance, digits=digits), "\n")
+    psum <- sum(x[[2]])
+    cat(names(x)[[2]], format(mean(psum), digits=digits), "\n")
+    cat("Penalized deviance:", format(deviance + psum, digits=digits), "\n")
+    invisible(x)
+}
+
+"-.dic" <- function(e1, e2)
+{
+    diffdic(e1, e2)
+}
+            
+"diffdic" <- function(dic1,dic2)
+{
+    if(!identical(names(dic1$deviance),names(dic2$deviance))) {
+        stop("incompatible dic objects: variable names differ")
+    }
+    if (!identical(dic1$type, dic2$type)) {
+        stop("incompatible dic object: different penalty types")
+    }
+    delta <- sapply(dic1$deviance, mean) + sapply(dic1$penalty, mean) -
+      sapply(dic2$deviance, mean) - sapply(dic2$penalty, mean)
+    class(delta) <- "diffdic"
+    return(delta)
+}
+
+"print.diffdic" <- function(x, ...)
+{
+    cat("Difference: ", sum(x), "\n", sep="") 
+    cat("Sample standard error: ", sqrt(length(x)) * sd(x), "\n", sep="")
+    invisible(x)
+}
---
+++ rjags/R/jags.object.R
@@ -0,0 +1,81 @@
+update.jags <- function(object, n.iter = 1, by, progress.bar, ...)
+{
+    if (!is.numeric(n.iter) || n.iter < 1) {
+        stop("Invalid n.iter")
+    }
+
+    adapting <- .Call("is_adapting", object$ptr(), PACKAGE="rjags")
+    on.exit(object$sync())
+
+    if (missing(progress.bar)) {
+        progress.bar <- getOption("jags.pb")
+    }
+    if (!is.null(progress.bar)) {
+        match.arg(progress.bar, c("text","gui","none"))
+        if (progress.bar=="none")
+            progress.bar <- NULL
+    }
+    
+    do.pb <- interactive() && !is.null(progress.bar) && n.iter >= 100
+    if (do.pb) {
+        start.iter <- object$iter()
+        end.iter <- start.iter + n.iter
+        pb <- switch(progress.bar,
+                     "text" = txtProgressBar(start.iter, end.iter,
+                     initial = start.iter, style=3, width=50,
+                     char=ifelse(adapting,"+","*")),
+                     "gui" = updatePB(start.iter, end.iter, adapting))
+    }
+    
+    ## Set refresh frequency for progress bar
+    if (missing(by) || by <= 0) {
+        by <- min(ceiling(n.iter/50), 100)
+    }
+    else {
+        by <- ceiling(by)
+    }
+    
+    ## Do updates
+    n <- n.iter
+    while (n > 0) {
+        .Call("update", object$ptr(), min(n,by), PACKAGE="rjags")
+        if (do.pb) {
+            switch(progress.bar,
+                   "text" = setTxtProgressBar(pb, object$iter()),
+                   "gui" =  setPB(pb, object$iter()))
+        }
+        n <- n - by
+    }
+    if (do.pb) {
+        close(pb)
+    }
+
+    invisible(NULL)
+}
+
+adapt <- function(object, n.iter, end.adaptation = FALSE, ...)
+{
+    if(.Call("is_adapting", object$ptr(), PACKAGE="rjags")) {
+        update(object, n.iter, ...)
+        ok <- .Call("check_adaptation", object$ptr(), PACKAGE="rjags")
+        if (end.adaptation) {
+            .Call("adapt_off", object$ptr(), PACKAGE="rjags")
+        }
+        return(ok)
+
+    }
+    else {
+        return(TRUE)
+    }
+}
+
+coef.jags <- function(object, chain = 1, ...) {
+    if (!is.numeric(chain) || chain < 1 || chain > object$nchain()) {
+        stop("Invalid chain")
+    }
+    object$state(internal=FALSE)[[chain]]
+}
+
+variable.names.jags <- function(object, ...) {
+    .Call("get_variable_names", object$ptr(), PACKAGE="rjags")
+}
