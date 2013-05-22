4643681aa3b605975f1b42baa36ca6476576a8d8
martyn <devnull@localhost>
2013-05-22 20:12:16 +0200

    Added LDA (Latent Dirichlet Allocation) sampler

---
+++ src/modules/mix/samplers/LDA.cc
@@ -0,0 +1,335 @@
+#include <config.h>
+
+#include "LDA.h"
+
+#include <rng/RNG.h>
+#include <graph/MixtureNode.h>
+#include <graph/MixTab.h>
+#include <graph/StochasticNode.h>
+#include <sampler/GraphView.h>
+#include <module/ModuleError.h>
+#include <sarray/RangeIterator.h>
+
+#include <set>
+#include <vector>
+
+using std::vector;
+using std::set;
+using std::string;
+
+namespace jags {
+
+    static bool isCat(StochasticNode const *snode) {
+	return snode->distribution()->name() == "dcat";
+    }
+
+    static bool isDirichlet(StochasticNode const *snode) {
+	return snode->distribution()->name() == "ddirch";
+    }
+
+    static int catSample(vector<double> const &probs, RNG *rng)
+    {
+	//Sample from an unnormalized discrete probability distribution
+	double sump = 0;
+	int i = 0;
+	int N = probs.size();
+	
+	for ( ; i < N; ++i) {
+	    sump += probs[i];
+	}
+	double p = sump * rng->uniform();    
+	
+	for (i = N - 1; i > 0; --i) {
+	    sump -= probs[i];
+	    if (sump <= p) 
+		break;
+	}
+	return i;
+    }
+
+    namespace mix {
+
+	LDA::LDA(vector<vector<StochasticNode*> > const &topics,
+		 vector<vector<StochasticNode*> > const &words,
+		 vector<StochasticNode*> const &topic_priors,
+		 vector<StochasticNode*> const &word_priors,
+		 GraphView const *gv, unsigned int ch)
+	    : _nTopic(word_priors.size()), 
+	      _nWord(word_priors[0]->length()),
+	      _nDoc(topics.size()), 
+	      _topicHyper(topic_priors[0]->parents()[0]->value(ch)), 
+	      _wordHyper(word_priors[0]->parents()[0]->value(ch)),
+	      _gv(gv), 
+	      _chain(ch),
+  	    _topicTokens(_nDoc), 
+	    _wordTokens(_nDoc), 
+	    _wordsByTopic(_nTopic, vector<int>(_nWord, 0)),
+	    _topicsByDoc(_nDoc, vector<int>(_nTopic, 0)),
+	    _docSums(_nDoc), _topicSums(_nTopic),
+	    _wordsObserved(true)
+	{
+	    //Set up tables
+	    for (unsigned int d = 0; d < _nDoc; ++d) {
+		_docSums[d] = topics[d].size();
+		for (unsigned int i = 0; i < _docSums[d]; ++i) {
+		    int topic = static_cast<int>(*topics[d][i]->value(ch)) - 1;
+		    _topicTokens[d].push_back(topic);
+		    _topicsByDoc[d][topic]++;
+		    _topicSums[topic]++;
+		    int word = static_cast<int>(*words[d][i]->value(ch)) - 1;
+		    _wordTokens[d].push_back(word);
+		    _wordsByTopic[topic][word]++;
+		    if (!isObserved(words[d][i])) _wordsObserved = false;
+		}
+	    }
+
+	    //Sanity check on gv: 
+	    vector<StochasticNode*> const &snodes = gv->nodes();
+	    unsigned int offset = 0;
+	    for (unsigned int doc = 0; doc < _nDoc; ++doc) {
+		for (unsigned int i = 0; i < _docSums[doc]; ++i) {
+		    if (topics[doc][i] != snodes[offset + i]) {
+			throwLogicError("Bad GraphView in LD constructor");
+		    }
+		}
+		offset += _docSums[doc];
+	    }
+	}
+
+	void LDA::rebuildTable()
+	{
+	    for (unsigned int t = 0; t < _nTopic; ++t) {
+		for (unsigned int w = 0; w < _nWord; ++w) {
+		    _wordsByTopic[t][w] = 0;
+		}
+	    }
+
+	    vector<StochasticNode *> const &s = _gv->nodes();
+	    unsigned int offset = 0;
+	    for (unsigned int d = 0; d < _nDoc; ++d) {
+		for (unsigned int i = 0; i < _docSums[d]; ++i) {
+		    int topic = _topicTokens[d][i];
+		    int word = static_cast<int>(*s[offset + i]->value(_chain)) 
+			- 1;
+		    _wordsByTopic[topic][word]++;
+		}
+		offset += _docSums[d];
+	    }
+	}
+
+	void LDA::update(RNG *rng)
+	{
+	    if (!_wordsObserved) {
+		rebuildTable();
+	    }
+
+	    double wordHyperSum = 0;
+	    for (unsigned int w = 0; w < _nWord; ++w) {
+		wordHyperSum += _wordHyper[w];
+	    }
+
+	    for (unsigned int doc = 0; doc < _nDoc; ++doc) {
+		for (unsigned int i = 0; i < _docSums[doc]; ++i) {
+
+		    //Find current topic and word values
+		    int &topic = _topicTokens[doc][i];
+		    int const &word = _wordTokens[doc][i];
+
+		    //Remove current values from tables
+		    _topicsByDoc[doc][topic]--;
+		    _wordsByTopic[topic][word]--;
+		    _topicSums[topic]--;
+
+		    //Calculate probability vector
+		    vector<double> prob(_nTopic);
+		    for (unsigned int t = 0; t < _nTopic; ++t) {
+			double prior = _topicsByDoc[doc][t] + _topicHyper[t];
+			double likelihood = 
+			    (_wordsByTopic[t][word] + _wordHyper[word]) /
+			    (_topicSums[t] + wordHyperSum);
+			prob[t] = prior * likelihood;
+		    }
+		    
+		    //Draw random sample from categorical distribution
+		    topic = catSample(prob, rng);
+
+		    //Restore current values to tables
+		    _topicsByDoc[doc][topic]++;
+		    _wordsByTopic[topic][word]++;
+		    _topicSums[topic]++;
+		}
+	    }
+
+	    vector<double> value;
+	    value.reserve(_gv->length());
+	    for (unsigned int d = 0; d < _nDoc; ++d) {
+		for (unsigned int i = 0; i < _docSums[d]; ++i) {
+		    value.push_back(_topicTokens[d][i] + 1);
+		}
+	    }
+	    _gv->setValue(value, _chain);
+	}
+
+	string LDA::name() const
+	{
+	    return "mix::LDA";
+	}
+	    
+	bool LDA::isAdaptive() const { return false; }
+	void LDA::adaptOff() { }
+	bool LDA::checkAdaptation() const { return true; }
+
+	bool LDA::canSample(vector<vector<StochasticNode*> > const &topics,
+			    vector<vector<StochasticNode*> > const &words,
+			    vector<StochasticNode*> const &topic_priors,
+			    vector<StochasticNode*> const &word_priors,
+			    Graph const &graph)
+	{
+	    //Set up dimensions
+	    unsigned int nDoc = topics.size();
+	    if (nDoc == 0) return false;
+	    if (words.size() != nDoc) return false;
+	    if (topic_priors.size() != nDoc) return false;
+
+	    unsigned int nTopic = word_priors.size();
+	    if (nTopic == 0) return false;
+	    
+	    unsigned int nWord = word_priors[0]->length();
+	    if (nWord == 0) return false;
+
+	    //topics and words are arranged hierarchically by document
+
+	    //topics and words have categorical distribution topics
+	    //must be unobserved (because we are sampling them) and
+	    //words must be observed (this may be relaxed in a future version).
+	    for (unsigned int d = 0; d < nDoc; ++d) {
+		if (topics[d].size() != words[d].size()) return false;
+		for (unsigned int i = 0; i < topics[d].size(); ++i) {
+		    if (!isCat(topics[d][i])) return false;
+		    if (!isCat(words[d][i])) return false;
+		    if (isObserved(topics[d][i])) return false;
+		    if (!isObserved(words[d][i])) return false;
+		}
+	    }
+	    
+	    //Word priors have a Dirichlet distribution.  They are
+	    //unobserved, as we need to marginalize over their
+	    //distribution
+	    for (unsigned int t = 0; t < nTopic; ++t) {
+		if (!isDirichlet(word_priors[t])) return false;
+		if (isObserved(word_priors[t])) return false;
+		if (word_priors[t]->length() != nWord) return false;
+	    }
+	    //Word priors have a common hyper-prior
+	    Node const *word_hyper = word_priors[0]->parents()[0];
+	    for (unsigned int t = 1; t < nTopic; ++t) {
+		if (word_priors[t]->parents()[0] != word_hyper) return false;
+	    }
+
+	    //Topic priors have a Dirichlet distribution. They are
+	    //unobserved, as we need to marginalize over their
+	    //distribution.
+	    for (unsigned int d = 0; d < nDoc; ++d) {
+		if (!isDirichlet(topic_priors[d])) return false;
+		if (isObserved(topic_priors[d])) return false;
+		if (topic_priors[d]->length() != nTopic) return false;
+	    }
+	    //Topic priors have a common hyper-prior
+	    Node const *topic_hyper = topic_priors[0]->parents()[0];
+	    for (unsigned int d = 1; d < nDoc; ++d) {
+		if (topic_priors[d]->parents()[0] != topic_hyper) return false;
+	    }
+
+	    //topics[d] is the stochastic children of topic_priors[d].
+	    //There are no intermediate deterministic nodes.
+	    for (unsigned int d = 0; d < nDoc; ++d) {
+		
+		GraphView tp_gv(topic_priors[d], graph);
+
+		if (!tp_gv.deterministicChildren().empty()) return false;
+
+		set<StochasticNode *> set1, set2;
+		set1.insert(tp_gv.stochasticChildren().begin(),
+			    tp_gv.stochasticChildren().end());
+		set2.insert(topics[d].begin(), topics[d].end());
+		if (set1 != set2) return false;
+	    }
+
+	    //The stochastic children of word_priors are in words[0]
+	    //... words[nDoc] 
+	    
+	    GraphView wp_gv(word_priors, graph);
+
+	    set<StochasticNode*> wp_schildren;
+	    wp_schildren.insert(wp_gv.stochasticChildren().begin(),
+				wp_gv.stochasticChildren().end());
+	    set<StochasticNode*> all_words;
+	    for (unsigned int d = 0; d < nDoc; ++d) {
+		all_words.insert(words[d].begin(), words[d].end());
+	    }
+	    if (wp_schildren != all_words) return false;
+
+	    //Create set to contain all mixture node children of word_priors
+	    set<MixtureNode const*> wp_mixnodes;
+
+	    //The intermediate deterministic nodes are all mixture
+	    //nodes with a common mixTab.
+	    vector<DeterministicNode*> const &wp_dchild = 
+		wp_gv.deterministicChildren();
+	    MixTab const *mtab = 0;
+	    for (unsigned int i = 0; i < wp_dchild.size(); ++i) {
+		MixtureNode const *m = asMixture(wp_dchild[i]);
+		if (m == 0) return false;
+		if (i == 0) {
+		    mtab = m->mixTab();
+		}
+		else if (m->mixTab() != mtab) {
+		    return false;
+		}
+		wp_mixnodes.insert(m);
+	    }
+
+	    //The only possible values of the mixTab are in word_priors
+	    set<Node const*> wp_set;
+	    wp_set.insert(word_priors.begin(), word_priors.end());
+	    for (RangeIterator p(mtab->range()); !p.atEnd(); p.nextLeft()) {
+		if (wp_set.count(mtab->getNode(p)) == 0) return false;
+	    }
+
+	    //Create set to contain all mixture node children of topics
+	    set<MixtureNode const*> topic_mixnodes;
+
+	    //Each topic is the index of a single mixture node and its
+	    //single stochastic child is the corresponding word. All
+	    //mixture nodes have the same mixTab
+	    for (unsigned int d = 0; d < nDoc; ++d) {
+		for (unsigned int i = 0; i < topics[d].size(); ++i) {
+		    
+		    GraphView topic_gv(topics[d][i], graph);
+
+		    vector<DeterministicNode*> topic_dchild = 
+			topic_gv.deterministicChildren();
+		    if (topic_dchild.size() != 1) return false;
+		    MixtureNode const *m = asMixture(topic_dchild[0]);
+		    if (m->index_size() != 1) return false;
+		    if (m->parents()[0] != topics[d][i]) return false;
+		    if (m->mixTab() != mtab) return false;
+
+		    topic_mixnodes.insert(m);
+
+		    vector<StochasticNode*> topic_schild = 
+			topic_gv.stochasticChildren();
+		    if (topic_schild.size() != 1) return false;
+		    if (topic_schild[0] != words[d][i]) return false;
+		}
+	    }
+
+	    //Finally, check that the deterministic children of topics
+	    //and of word_priors are the same
+	    if (wp_mixnodes != topic_mixnodes) return false;
+
+	    return true;
+	}
+    }
+}
+
---
+++ src/modules/mix/samplers/LDA.h
@@ -0,0 +1,89 @@
+#ifndef LDA_H_
+#define LDA_H_
+
+#include <sampler/SampleMethod.h>
+
+#include <vector>
+
+namespace jags {
+    
+    class Graph;
+    class GraphView;
+    class StochasticNode;
+    
+    namespace mix {
+	/**
+	 * @short Collapsed sampler for Latent Dirichlet Allocation
+	 * models.
+	 */
+	class LDA : public SampleMethod {
+	    const unsigned int _nTopic, _nWord, _nDoc;
+	    double const *_topicHyper;
+	    double const *_wordHyper;
+	    GraphView const *_gv;
+	    const unsigned int _chain;	    
+	    std::vector<std::vector<int> > _topicTokens, _wordTokens;
+	    std::vector<std::vector<int> > _wordsByTopic, _topicsByDoc;
+	    std::vector<unsigned int> _docSums, _topicSums;
+	    bool _wordsObserved;
+	    void rebuildTable();
+	  public:
+	    /**
+	     * Constructor.
+	     * 
+	     * @param topics Topic indicators with a categorical
+	     * distribution, organized hierarchically within
+	     * documents. Hence topics.size() is nDoc, the number of
+	     * documents, and topics[d] contains the topic indicators
+	     * for document d.  These are the nodes that are sampled
+	     * by the LDA sampler.
+	     * 
+	     * @param words Word indicators with a categorical
+	     * distribution, organized in the same way as
+	     * topics. Hence words[d][i] is the stochastic child of
+	     * topics[d][i]. All words must be observed.
+	     *
+	     * @param topic_priors Vector of length nDoc containing
+	     * Dirichlet priors for the topic indicators, one for each
+	     * document. All topic priors share a common hyper-prior.
+	     *
+	     * @param word_priors Vector of length nTopic, the number
+	     * of topics, containing Dirichlet priors for the
+	     * words. The choice of which element of word_prior to use
+	     * as prior for words[d][i] is determined by the value of
+	     * topics[d][i]. All word priors share a common
+	     * hyper-prior.
+	     *
+	     * @param gv Pointer to the GraphView within which sampling
+	     * takes place. 
+	     */
+	    LDA(std::vector<std::vector<StochasticNode*> > const &topics,
+		std::vector<std::vector<StochasticNode*> > const &words,
+		std::vector<StochasticNode*> const &topic_priors,
+		std::vector<StochasticNode*> const &word_priors,
+		GraphView const *gv, unsigned int chain);
+	    void update(RNG *rng);
+	    /**
+	     * LDA is not adaptive
+	     */
+	    bool isAdaptive() const;
+	    void adaptOff();
+	    bool checkAdaptation() const;
+	    std::string name() const;
+	    /**
+	     * Tests whether a set of topics can be sampled by the LDA
+	     * method. The arguments are the same as for the LDA
+	     * constructor, except that a Graph is passed as the last
+	     * argument instead of a GraphView.
+	     */
+	    static bool canSample(
+		std::vector<std::vector<StochasticNode*> > const &topics,
+		std::vector<std::vector<StochasticNode*> > const &words,
+		std::vector<StochasticNode*> const &topic_priors,
+		std::vector<StochasticNode*> const &word_priors,
+		Graph const &graph);
+	};
+    } 
+}
+
+#endif /* LDA_H_ */
