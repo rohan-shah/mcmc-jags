e80dc02b8dc6610be967301bfe0013f07b6c6b49
martyn <devnull@localhost>
2014-10-27 21:01:49 +0100

    Added missing Combine function.

---
+++ src/modules/bugs/functions/Combine.cc
@@ -0,0 +1,65 @@
+#include <config.h>
+#include "Combine.h"
+
+#include <algorithm>
+#include <numeric>
+
+#include <util/dim.h>
+#include <util/logical.h>
+
+using std::vector;
+using std::copy;
+using std::accumulate;
+
+namespace jags {
+    namespace bugs {
+
+	Combine::Combine ()
+	    : VectorFunction ("c", 0)
+	{
+	}
+
+	void Combine::evaluate (double *value, 
+				vector <double const *> const &args,
+				vector<unsigned int> const &lengths) const
+	{
+	    for (unsigned int i = 0; i < args.size(); ++i) {
+		value = copy(args[i], args[i] + lengths[i], value);
+	    }
+	}
+
+	unsigned int Combine::length (vector<unsigned int> const &lens,
+				      vector<double const *> const &vals) const
+	{
+	    return accumulate(lens.begin(), lens.end(), 0U);
+	}
+
+	bool Combine::isDiscreteValued(vector<bool> const &mask) const
+	{
+	    return allTrue(mask);
+	}
+
+	bool Combine::isScale(vector<bool> const &mask, 
+			      vector<bool> const &fixed) const
+	{
+	    //The Combine function behaves like an aggregate node
+	    //and so has the same rules for preserving scale functions
+	    return allTrue(mask);
+	}
+
+	bool Combine::isLinear(vector<bool> const &mask, 
+			       vector<bool> const &fixed) const
+	{
+	    //The Combine function behaves like an aggregate node
+	    //and so has the same rules for preserving linear functions
+	    return true;
+	}
+
+	bool Combine::checkParameterLength(vector<unsigned int> const &args)
+	    const
+	{
+	    return true;
+	}
+    }
+
+}
---
+++ src/modules/bugs/functions/Combine.h
@@ -0,0 +1,39 @@
+#ifndef FUNC_COMBINE_H_
+#define FUNC_COMBINE_H_
+
+#include <function/VectorFunction.h>
+
+namespace jags {
+    namespace bugs {
+
+	/**
+	 * @short Combine values into a vector
+	 *
+	 * Concatenates all arguments into a single vector
+	 * <pre>
+	 * y <- c(x[],y, z[,])
+	 * </pre>
+	 */
+	class Combine : public VectorFunction
+	{
+	  public:
+	    Combine ();
+	    void evaluate(double *value, 
+			  std::vector <double const *> const &args,
+			  std::vector <unsigned int> const &lengths) const;
+	    unsigned int 
+		length(std::vector<unsigned int> const &parlengths,
+		       std::vector<double const *> const &parvalues) const;
+	    bool isDiscreteValued(std::vector<bool> const &mask) const;
+	    bool isScale(std::vector<bool> const &mask,
+			 std::vector<bool> const &isfixed) const;
+	    bool isLinear(std::vector<bool> const &mask,
+			  std::vector<bool> const &isfixed) const;
+	    bool checkParameterLength(std::vector<unsigned int> const &args)
+		const;
+	};
+
+    }
+}
+
+#endif /* FUNC_COMBINE_H_ */
