bff54f030ab4c86395ebf82091295357df2581dc
martyn_plummer <martyn_plummer@sourceforge.net>
2016-03-20 17:52:11 +0100

    Improve efficiency of Holmes-Held algorithm by minimizing memory allocation.

--- src/modules/glm/samplers/HolmesHeld.cc
+++ src/modules/glm/samplers/HolmesHeld.cc
@@ -22,9 +22,39 @@ using std::sqrt;
 
 extern cholmod_common *glm_wk;
 
+static cholmod_sparse shallow_copy(cholmod_sparse *x, unsigned int c)
+{
+    //Take a copy of column c of sparse matrix x without allocating
+    //any memory. This is computationally cheaper than calling
+    //cholmod_submatrix, but potentially dangerous if the copy is
+    //passed to a function that tries to modify it. Note that we can
+    //only have one shallow copy in use at a time since they all share
+    //the same array of column pointers given by the static array p.
+
+    static int p[2]; 
+    
+    cholmod_sparse xcopy = *x;
+
+    double *xx = static_cast<double*>(x->x);
+    int *xp = static_cast<int*>(x->p);
+    int *xi = static_cast<int*>(x->i);
+
+    xcopy.ncol = 1;
+    int nz = xp[c+1] - xp[c];
+    xcopy.nzmax = nz;
+    p[0] = 0;
+    p[1] = nz; 
+    xcopy.p = &p;
+    xcopy.i = xi + xp[c];
+    xcopy.x = xx + xp[c];
+
+    return xcopy;
+}
+
 namespace jags {
 namespace glm {
 
+    
     HolmesHeld::HolmesHeld(GraphView const *view,
 			   vector<SingletonGraphView const *> const &sub_views,
 			   vector<Outcome *> const &outcomes,
@@ -33,7 +63,7 @@ namespace glm {
     {
     }
 
-    void HolmesHeld::updateAuxiliary(cholmod_dense *w, 
+    void HolmesHeld::updateAuxiliary(cholmod_dense *W, 
 				     cholmod_factor *N, RNG *rng)
     {
 	/* 
@@ -60,11 +90,11 @@ namespace glm {
 
 	//Transpose and permute the design matrix
 	cholmod_sparse *t_x = cholmod_transpose(_x, 1, glm_wk);
-	int *fperm = static_cast<int *>(_factor->Perm);
-	cholmod_sparse *Pt_x = cholmod_submatrix(t_x, fperm, t_x->nrow,
+	int *fperm = static_cast<int*>(_factor->Perm);
+	cholmod_sparse *pt_x = cholmod_submatrix(t_x, fperm, t_x->nrow,
 						 0, -1, 1, 1, glm_wk);
 	cholmod_free_sparse(&t_x, glm_wk);
-
+	
 	int ncol = _x->ncol;
 	vector<double> d(ncol, 1);
 	if (!_factor->is_ll) {
@@ -77,33 +107,41 @@ namespace glm {
 	    }
 	}
 
-	double *wx = static_cast<double *>(w->x);
+	double *Wx = static_cast<double *>(W->x);
 	
 	cholmod_dense *U = 0, *Y = 0, *E = 0;
-	cholmod_sparse *Uset = 0;
+	cholmod_sparse *uset = 0;
 
 	cholmod_dense *X = cholmod_allocate_dense(ncol, 1, ncol,
 						  CHOLMOD_REAL, glm_wk);
 	double *Xx = static_cast<double*>(X->x);
-	
+
 	for (int r = 0; r < nrow; ++r) {
 
+	    // We need to do this loop with minimal memory
+	    // allocation. The cholmod_solve2 function uses a
+	    // dense/sparse pair X/xset for input and another
+	    // dense/sparse pair U/uset for output: U, uset, and
+	    // workspaces Y and E are allocated on first use and then
+	    // reused in subsequent calls (See CHOLMOD
+	    // documentation). We also use the utility function
+	    // shallow_copy to access each column of pt_x without
+	    // further memory allocation.
+	    
 	    if (_outcomes[r]->fixedb()) continue;
 
-	    //Get row r of design matrix
-	    cholmod_sparse *Xset = cholmod_submatrix(Pt_x, NULL, -1,
-						     &r, 1, 1, 1, glm_wk);
+	    cholmod_sparse xset = shallow_copy(pt_x, r);
+	    double *xx = static_cast<double*>(xset.x);
+	    int *xp = static_cast<int*>(xset.p);
+	    int *xi = static_cast<int*>(xset.i);
 
-	    double *xsx = static_cast<double*>(Xset->x);
-	    int *xsp = static_cast<int*>(Xset->p);
-	    int *xsi = static_cast<int*>(Xset->i);
-
-	    for (int j = 0; j < xsp[1]; ++j) {
-		int c = xsi[j];
-		Xx[c] = xsx[j];
+	    //Copy non-zero elements of column r to dense vector X
+	    for (int j = 0; j < xp[1]; ++j) {
+		int c = xi[j];
+		Xx[c] = xx[j];
 	    }
 	    
-	    cholmod_solve2(CHOLMOD_L, _factor, X, Xset, &U, &Uset, &Y, &E,
+	    cholmod_solve2(CHOLMOD_L, _factor, X, &xset, &U, &uset, &Y, &E,
 			   glm_wk);
 
 	    double mu_r = _outcomes[r]->mean(); // See IMPORTANT NOTE above
@@ -114,20 +152,18 @@ namespace glm {
 	    double zr_mean = 0;
 	    double gr = 0; 
 
-	    int *up = static_cast<int*>(Uset->p);
-	    int *ui = static_cast<int*>(Uset->i);
-	    double *ux = static_cast<double*>(U->x);
+	    int *up = static_cast<int*>(uset->p);
+	    int *ui = static_cast<int*>(uset->i);
+	    double *Ux = static_cast<double*>(U->x);
 
 	    for (int j = 0; j < up[1]; ++j) {
 		int c = ui[j];
-		zr_mean  += ux[c] * wx[c] / d[c]; 
-		gr  += ux[c] * ux[c] / d[c];
+		zr_mean  += Ux[c] * Wx[c] / d[c]; 
+		gr  += Ux[c] * Ux[c] / d[c];
 	    }
 
 	    double Hr = gr * tau_r; // diagonal of hat matrix
 	    if (1 - Hr <= 0) {
-		// Theoretically this should never happen, but
-		// numerical instability may cause it
 		continue;
 	    }
 	    zr_mean -= Hr * (_outcomes[r]->value() - mu_r);
@@ -141,16 +177,15 @@ namespace glm {
 	    double zdelta = (_outcomes[r]->value() - zold) * tau_r;
 	    for (int j = 0; j < up[1]; ++j) {
 		int c = ui[j];
-		wx[c] += ux[c] * zdelta; 
+		Wx[c] += Ux[c] * zdelta; 
 	    }
 
-	    cholmod_free_sparse(&Xset, glm_wk);
 	}
 	    
 	//Free workspace
 
-	cholmod_free_sparse(&Pt_x, glm_wk);
-	cholmod_free_sparse(&Uset, glm_wk);
+	cholmod_free_sparse(&pt_x, glm_wk);
+	cholmod_free_sparse(&uset, glm_wk);
 	
 	cholmod_free_dense(&U, glm_wk);
 	cholmod_free_dense(&Y, glm_wk);
